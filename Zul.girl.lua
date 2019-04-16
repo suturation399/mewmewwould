3040b591ce55696b87adbc7ef52b65d228929905
suturation399 <33090516+suturation399@users.noreply.github.com>
2019-04-17 01:23:11 +0800

    Create Zul.girl.lua

---
+++ Zul.girl.lua
@@ -0,0 +1,41 @@
+function ondraw()
+  local drids = mount:getdrids()
+  local round = game:getround()
+  local extra = game:getextraround()
+  local hands = game:gethand(self)
+  local handr = game:gethand(self:right())
+  local handc = game:gethand(self:cross())
+  local handl = game:gethand(self:left())
+  local eaa = (round + 1) * 20 + extra * 7
+
+  if who ~= self or rinshan then
+    return
+  end
+
+  if who == self and hands:ismenzen() then
+    for _, t in ipairs(hands:effa()) do
+      mount:lighta(t, eaa)
+    end
+      if handr:step() <= 1 then 
+      for _, t in ipairs(handr:effa()) do
+         print("下家向聽數", handr:step(),"有效牌", t)
+     end
+  end
+
+      if handc:step() <= 1 then 
+      for _, t in ipairs(handc:effa()) do
+         print("對家向聽數", handc:step(),"有效牌", t)
+     end
+  end
+
+      if handl:step() <= 1 then 
+      for _, t in ipairs(handl:effa()) do
+         print("對家向聽數", handl:step(),"有效牌", t)
+     end
+  end
+end
+
+   for _, t in ipairs(hands:effa()) do
+      mount:lighta(t, -10)
+  end
+end
