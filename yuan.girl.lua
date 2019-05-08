12d2b69d945c00b384841bac8c74644d8a349c75
suturation399 <33090516+suturation399@users.noreply.github.com>
2019-05-08 12:02:40 +0800

    Update yuan.girl.lua

--- yuan.girl.lua
+++ yuan.girl.lua
@@ -1,15 +1,22 @@
 function onmonkey()
   junme = 0
+  local exist = exists[self:index()]
+  
+  exist:incmk(T37.new("0p"), 35)
+  exist:incmk(T37.new("0s"), 35)
+  exist:incmk(T37.new("0m"), 35)
 end
 
 function ondraw()
-  local hand = game:gethand(who)
-  local effas = hand:effa()
+  local hands = game:gethand(self)
+  local steps = hands:step(self)
   local handr = game:gethand(self:right())
   local handc = game:gethand(self:cross())
   local handl = game:gethand(self:left())
+  local drids = mount:getdrids()
   local junmk = junme * 3
   local sunmk = junme * 30
+  local jdomk = 90 - junme * 5
 
   if who ~= self or rinshan then
     return
@@ -17,9 +24,23 @@ function ondraw()
  
   if who == self then
     junme = junme + 1
-      for _, t in ipairs(hand:effa()) do
+    if steps >= 1 then
+      for _, t in ipairs(hands:effa()) do
         mount:lighta(t, junmk)
       end
+      mount:lighta(T37.new("0p"), jdomk)
+      mount:lighta(T37.new("0s"), jdomk)
+      mount:lighta(T37.new("0m"), jdomk)
+      for _, t in ipairs(drids) do
+        mount:lighta(t:dora(), jdomk)
+      end
+    end
+    
+    if steps == 0 then
+      for _, t in ipairs(hands:effa()) do
+        mount:lighta(t, -6)
+      end
+    end
 
     if handr:ready() then
       for _, t in ipairs(handr:effa()) do
