f19da442495959190d240ef2cf5adc892dfcc485
suturation399 <33090516+suturation399@users.noreply.github.com>
2019-05-22 01:50:08 +0800

    Update testing.girl.lua

--- testing.girl.lua
+++ testing.girl.lua
@@ -1,9 +1,42 @@
 blizzard = 0
 ice = 30
 
+coldtars = {
+  "2p", "4p", "8p", "2s", "3s", "4s", "6s", "8s", "9m", "1f", "2f", "3f", "4f", "1y", "2y" 
+}
+
+coldpowers = {
+  [coldtars] = 60
+}
+
 function onmonkey()
   junme = 0
+  local exist = exists[self:index()]
+  local sw = game:getselfwind(self)
+  local rw = game:getroundwind()
+  
+  if sw == 1 and rw == 1 then
+    for i = 1, 4 do
+      exist:incmk(T34.new(i .. "f"), 63)
+    end
+  end
+  
+  if sw == 1 and rw == 2 then
+    exist:incmk(T34.new("1y"), 99)
+  end
 end
+
+function checkinit()
+  local sw = game:getselfwind(self)
+  local rw = game:getroundwind()
+  
+  if who ~= self or rw ~= 2 or iter > 50 or (rw == 2 and sw ~= 1) then
+    return true
+  end
+  
+  return init:ct(T34.new("1y")) == 3
+end
+  
 
 function ondraw()
   local round = game:getround()
@@ -12,7 +45,7 @@ function ondraw()
   local drids = mount:getdrids()
   
   for _, t in ipairs(drids) do
-    nd = hand:ct(T37.new("0p")) + hand:ct(T37.new("0s")) + hand:ct(T37.new("0m")) + hand:ct(t:dora())
+    nd = hand:ctaka5() + hand:ct(t:dora())
   end
   
   if who == self then
