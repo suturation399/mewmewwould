5a716bdf78e914263030aa02d6df808f4aff8b18
suturation399 <33090516+suturation399@users.noreply.github.com>
2019-05-26 00:10:56 +0800

    Update testing.girl.lua

--- testing.girl.lua
+++ testing.girl.lua
@@ -1,37 +1,56 @@
-lv5tars = {
-  "1f", "2f", "3f", "4f", "1y"
-}
-
-lv4tars = {
-  "2p", "4p", "8p", "2y"
-}
-
-lv3tars = {
-  "2s", "3s", "4s", "6s", "8s", "2y"
-}
-
-powers = {
-  [lv5tars] = 99,
-  [lv4tars] = 66,
-  [lv3tars] = 33
+coldtars = {
+  "2p", "4p", "8p", "2s", "3s", "4s", "6s", "8s", "1f", "2f", "3f", "4f", "1y", "2y" 
 }
 
 function onmonkey()
-  for tars, mk in pairs(powers) do
-    for _, t in ipairs(tars) do
-      exists[self:index()]:incmk(T34.new(t), mk)
-    end
+  for _, t in ipairs(coldtars) do
+    exists[self:index()]:incmk(T34.new(t), 93)
   end
 end
 
 function ondraw()
-  if who ~= self or rinshan then
+  local hand = game:gethand(who)
+  local shand = game:gethand(self)
+  local handr = game:gethand(self:right())
+  local handc = game:gethand(self:cross())
+  local handl = game:gethand(self:left())
+  local seffas = shand:effa()
+  local effas = hand:effa()
+  
+  if rinshan then
     return
   end
 
-  for tars, mk in pairs(powers) do
-    for _, t in ipairs(tars) do
-      mount:lighta(T34.new(t), mk)
+  if who == self then
+    for _, t in ipairs(coldtars) do
+      mount:lighta(T34.new(t), 93)
+    end
+    if handr:ready() then 
+      for _, t in ipairs(handr:effa()) do
+         mount:lighta(t, -133)
+      end
+    end
+    if handc:ready() then 
+      for _, t in ipairs(handc:effa()) do
+         mount:lighta(t, -133)
+      end
+    end
+    if handl:ready() then 
+      for _, t in ipairs(handl:effa()) do
+        mount:lighta(t, -133)
+      end
+    end
+  end
+  
+  if who ~= self then
+    if shand:step() ~= 0 then
+      for _, t in ipairs(effas) do
+        mount:lighta(t, 93)
+      end
+    else
+      for _, t in ipairs(seffas) do
+        mount:lighta(t, 93)
+      end
     end
   end
 end
