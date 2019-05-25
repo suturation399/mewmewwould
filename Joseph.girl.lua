4d1ba0464a0de0e8103b2e2f24eb7026c7447be5
suturation399 <33090516+suturation399@users.noreply.github.com>
2019-05-26 00:11:55 +0800

    Create Joseph.girl.lua

---
+++ Joseph.girl.lua
@@ -0,0 +1,56 @@
+coldtars = {
+  "2p", "4p", "8p", "2s", "3s", "4s", "6s", "8s", "1f", "2f", "3f", "4f", "1y", "2y" 
+}
+
+function onmonkey()
+  for _, t in ipairs(coldtars) do
+    exists[self:index()]:incmk(T34.new(t), 93)
+  end
+end
+
+function ondraw()
+  local hand = game:gethand(who)
+  local shand = game:gethand(self)
+  local handr = game:gethand(self:right())
+  local handc = game:gethand(self:cross())
+  local handl = game:gethand(self:left())
+  local seffas = shand:effa()
+  local effas = hand:effa()
+  
+  if rinshan then
+    return
+  end
+
+  if who == self then
+    for _, t in ipairs(coldtars) do
+      mount:lighta(T34.new(t), 93)
+    end
+    if handr:ready() then 
+      for _, t in ipairs(handr:effa()) do
+         mount:lighta(t, -133)
+      end
+    end
+    if handc:ready() then 
+      for _, t in ipairs(handc:effa()) do
+         mount:lighta(t, -133)
+      end
+    end
+    if handl:ready() then 
+      for _, t in ipairs(handl:effa()) do
+        mount:lighta(t, -133)
+      end
+    end
+  end
+  
+  if who ~= self then
+    if shand:step() ~= 0 then
+      for _, t in ipairs(effas) do
+        mount:lighta(t, 93)
+      end
+    else
+      for _, t in ipairs(seffas) do
+        mount:lighta(t, 93)
+      end
+    end
+  end
+end
