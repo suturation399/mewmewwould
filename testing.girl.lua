0cbe972a360c653ee10dcecd2d551e7768b4c7b5
suturation399 <33090516+suturation399@users.noreply.github.com>
2019-06-06 01:31:46 +0800

    Update testing.girl.lua

--- testing.girl.lua
+++ testing.girl.lua
@@ -1,34 +1,55 @@
-function onmonkey()
+function checkinit()
   junme = 0
+  local selfwind = game:getselfwind(self)
+  local inp = init:closed():ct("p")
+  local inm = init:closed():ct("m")
+  local ins = init:closed():ct("s")
+  
+  if who ~= self or iter > 4 then
+    return true
+  end
+
+  if who == self and selfwind == 1 then
+    return init:step() == 3 and inp == inm and inm == ins
+  end
+
+  if who == self and selfwind ~= 1 then
+    return init:step() >= 3 and init:step() <= 4 and inp == inm and inm == ins
+  end
 end
 
 function ondraw()
-  local hand = game:gethand(who)
-  local shand = game:gethand(self)
-  local handr = game:gethand(self:right())
-  local handc = game:gethand(self:cross())
-  local handl = game:gethand(self:left())
-  local seffas = shand:effa()
-  local effas = hand:effa()
-  local torment = (12 - junme) * 50
+  local drids = mount:getdrids()
+  local hands = game:gethand(self)
+  local steps = hands:step(self)
+  local junmk = junme * 4
+
+  for _, t in ipairs(drids) do
+    nd = hands:ctaka5() + hands:ct(t:dora())
+  end
+
+  local dormk = (nd - 2) * -84 - 14
   
-  if rinshan then
+  if who ~= self or rinshan then
     return
   end
 
   if who == self then
     junme = junme + 1
-  end
-  
-  if who == self:right() then
-    if handr:step() == 1 then
-      for _, t in ipairs(handr:effa()) do
-        mount:lighta(t, torment)
+    mount:lighta(T37.new("0p"), dormk)
+    mount:lighta(T37.new("0s"), dormk)
+    mount:lighta(T37.new("0m"), dormk)
+    for _, t in ipairs(drids) do
+      mount:lighta(t:dora(), dormk)
+    end
+    if steps >= 1 then
+      for _, t in ipairs(hands:effa()) do
+        mount:lighta(t, junmk)
+      end
+    else
+      for _, t in ipairs(hands:effa()) do
+        mount:lighta(t, 4)
       end
     end
-    if handl:step() == 0 then
-      for _, t in ipairs(handl:effa()) do
-        mount:lighta(t, 90)
-      end
-    end
   end
+end
