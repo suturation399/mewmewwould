4a75bea9a1509f90c96d0a90ab29fd82d0cc2480
suturation399 <33090516+suturation399@users.noreply.github.com>
2019-07-25 20:00:25 +0800

    Update Azshara.girl.lua

--- Azshara.girl.lua
+++ Azshara.girl.lua
@@ -2,15 +2,31 @@ decree = 0
 failurer = 0
 failurec = 0
 failurel = 0
-power = 0
 pressure = 0
 catastrophe = 0
+junme = 0
 
 function onmonkey()
-  ancientr = failurer + 30
-  ancientc = failurer + 30
-  ancientl = failurel + 30
-  power = 30
+  power = 0
+  junme = 0
+  
+  if failurer == 1 then
+    ancientr = -100
+  else
+    ancientr = 30
+  end
+  
+  if failurec == 1 then
+    ancientc = -100
+  else
+    ancientc = 30
+  end
+  
+    if failurel == 1 then
+    ancientl = -100
+  else
+    ancientl = 30
+  end
   
   if catastrophe == 1 then
     for i = 1, 9 do
@@ -20,17 +36,31 @@ function onmonkey()
 end
      
 function checkinit()
-  local ok = 1
-  
   if who ~= self then
-    return true
+    if pressure ~= 0 then
+      return init:step() >= pressure + 3
+    else
+      return true
+    end
   end
 
   if catastrophe == 1 then
     return init:closed():ct("p") == 13
   else
-    
+    return true
+  end
 end
 
 function ondraw()
+  local riverr = game:getriver(self:right())
+  local riverc = game:getriver(self:cross())
+  local riverl = game:getriver(self:left())
+  local hands = game:gethand(self)
   
+  if who ~= self or rinshan then
+    return
+  end
+  
+  if who == self then
+    junme = junme + 1
+    
