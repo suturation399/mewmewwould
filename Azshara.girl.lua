a1633c913b2b5e88590849be1f76c106b38a9dab
suturation399 <33090516+suturation399@users.noreply.github.com>
2019-07-25 14:01:42 +0800

    Update Azshara.girl.lua

--- Azshara.girl.lua
+++ Azshara.girl.lua
@@ -1,15 +1,36 @@
-a = 1103515245
-c = 12345
-m = 2 ^ 32 - 1
-seed = 1
-
-function random()
-  seed = (a * seed + c) % m
-  return seed
-end
+decree = 0
+failurer = 0
+failurec = 0
+failurel = 0
+power = 0
+pressure = 0
+catastrophe = 0
 
 function onmonkey()
-  status = random() % 6
+  ancientr = failurer + 30
+  ancientc = failurer + 30
+  ancientl = failurel + 30
+  power = 30
+  
+  if catastrophe == 1 then
+    for i = 1, 9 do
+      exists[self:index()]:incmk(T34.new(i .. "p"), 200)
+    end
+  end
+end
+     
+function checkinit()
+  local ok = 1
+  
+  if who ~= self then
+    return true
+  end
+
+  if catastrophe == 1 then
+    return init:closed():ct("p") == 13
+  else
+    
 end
 
-
+function ondraw()
+  
