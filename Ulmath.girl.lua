19fdfd21dad835599dfa2b43c080343e07531ff5
suturation399 <33090516+suturation399@users.noreply.github.com>
2019-09-04 01:35:20 +0800

    Update Ulmath.girl.lua

--- Ulmath.girl.lua
+++ Ulmath.girl.lua
@@ -19,10 +19,10 @@ function ondraw()
   if who ~= self then
     if hand:step() <= hands:step() then
       for i = 1, 4 do
-        exist:incmk(T34.new(i .. "f"), 55)
+        mount:lighta(T34.new(i .. "f"), 55)
       end
       for i = 1, 3 do
-        exist:incmk(T34.new(i .. "y"), 55)
+        mount:lighta(T34.new(i .. "y"), 55)
       end
       for _, t in ipairs(hand:effa()) do
         mount:lighta(t, -55)
@@ -37,7 +37,7 @@ function ondraw()
   junme = junme + 1
   if hands:step() > handr:step() or hands:step() > handc:step() or hands:step() > handl:step() then
     for _, t in ipairs(hand:effa7()) do
-      mount:lighta(t, junme * 15)
+      mount:lighta(t, junme * 5 * 2)
     end
   else
     for _, t in ipairs(hand:effa7()) do
