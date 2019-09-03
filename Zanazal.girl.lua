d2abdb65ff89ee2bdce48d00b026a414c617e103
suturation399 <33090516+suturation399@users.noreply.github.com>
2019-09-03 13:05:17 +0800

    Update Zanazal.girl.lua

--- Zanazal.girl.lua
+++ Zanazal.girl.lua
@@ -1,7 +1,7 @@
 doge = 0
 
 function ondice()
-  doge = rand:gen(25)
+  doge = rand:gen(20)
 end
 
 function onmonkey()
@@ -90,9 +90,9 @@ function onmonkey()
   
   if status >= 6 then
     for i = 2, 8 do
-      exist:incmk(T34.new(i .. "p"), 44)
-      exist:incmk(T34.new(i .. "s"), 44)
-      exist:incmk(T34.new(i .. "m"), 44)
+      exist:incmk(T34.new(i .. "p"), 5)
+      exist:incmk(T34.new(i .. "s"), 5)
+      exist:incmk(T34.new(i .. "m"), 5)
     end
   end
 end
@@ -106,47 +106,58 @@ function checkinit()
     return true
   end
 
-  if status == 0 then
-    return init:step() >= 3 and init:step() <= 4 and init:ct(T34.new("1y")) == 2
-  end
-
   if status == 1 then
-    return init:step() >= 3 and init:step() <= 4 and init:ct(T34.new("2y")) == 2
+    return init:step() <= 4 and init:ct(T34.new("1y")) == 2
   end
 
   if status == 2 then
-    return init:step() >= 3 and init:step() <= 4 and init:ct(T34.new("3y")) == 2
+    return init:step() <= 4 and init:ct(T34.new("2y")) == 2
   end
 
   if status == 3 then
-    if sw == 1 then
-      return init:step() >= 3 and init:step() <= 4 and init:ct(T34.new("1f")) == 2
-    end
-    if sw == 2 then
-      return init:step() >= 3 and init:step() <= 4 and init:ct(T34.new("2f")) == 2
-    end
-    if sw == 3 then
-      return init:step() >= 3 and init:step() <= 4 and init:ct(T34.new("3f")) == 2
-    end
-    if sw == 4 then
-      return init:step() >= 3 and init:step() <= 4 and init:ct(T34.new("4f")) == 2
-    end
+    return init:step() <= 4 and init:ct(T34.new("3y")) == 2
   end
 
   if status == 4 then
-    if rw == 1 then
-      return init:step() >= 3 and init:step() <= 4 and init:ct(T34.new("1f")) == 2
+    if sw == 1 then
+      return init:step() <= 4 and init:ct(T34.new("1f")) == 2
     end
-    if rw == 2 then
-      return init:step() >= 3 and init:step() <= 4 and init:ct(T34.new("2f")) == 2
+    if sw == 2 then
+      return init:step() <= 4 and init:ct(T34.new("2f")) == 2
     end
-    if rw == 3 then
-      return init:step() >= 3 and init:step() <= 4 and init:ct(T34.new("3f")) == 2
+    if sw == 3 then
+      return init:step() <= 4 and init:ct(T34.new("3f")) == 2
+    end
+    if sw == 4 then
+      return init:step() <= 4 and init:ct(T34.new("4f")) == 2
     end
   end
 
-  if status >= 5 then
-  local ny = init:ct(T34.new("1m")) + init:ct(T34.new("9m")) + init:ct(T34.new("1p")) + init:ct(T34.new("9p")) + init:ct(T34.new("1s")) + init:ct(T34.new("9s")) + init:ct(T34.new("1f")) + init:ct(T34.new("2f")) + init:ct(T34.new("3f")) + init:ct(T34.new("4f")) + init:ct(T34.new("1y")) + init:ct(T34.new("2y")) + init:ct(T34.new("3y"))
-  return ny <= 3 and init:step() >= 3 and init:step() <= 4
+  if status == 5 then
+    if rw == 1 then
+      return init:step() <= 4 and init:ct(T34.new("1f")) == 2
+    end
+    if rw == 2 then
+      return init:step() <= 4 and init:ct(T34.new("2f")) == 2
+    end
+    if rw == 3 then
+      return init:step() <= 4 and init:ct(T34.new("3f")) == 2
+    end
+  end
+
+  if status >= 6 then
+  return true
   end
 end
+
+function ondraw()
+  local drids = mount:getdrids()
+  local hands = game:gethand(self)
+  local steps = hands:step(self)
+  
+  if who ~= self or rinshan then
+    return
+  end
+  
+  if who == self then
+    junme = junme + 1
