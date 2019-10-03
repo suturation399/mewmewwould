d76105805b27812b30536761055362cd1230406c
suturation399 <33090516+suturation399@users.noreply.github.com>
2019-10-03 19:04:12 +0800

    Update Uunat.girl.lua

--- Uunat.girl.lua
+++ Uunat.girl.lua
@@ -1,9 +1,58 @@
-function checkinit()
-  junme = 0
+function onmonkey()
+  junme = 1
+end
+
+function ondraw()
+  local hand = game:gethand(who)
+  local hands = game:gethand(self)
+  local handr = game:gethand(self:right())
+  local handc = game:gethand(self:cross())
+  local handl = game:gethand(self:left())
+  local effas = hand:effa()
   
-  if who ~= self or iter > 222 then
-    return true
+  if who ~= self then
+    if hand:step4() <= hand:step7() and hand:step4() <= hand:step13() then
+      for _, t in ipairs(hand:effa()) do
+        mount:lighta(t, junme * 7)
+      end
+      for _, t in ipairs(hand:effa4()) do
+        mount:lighta(t, junme * -7)
+      end
+    else
+      for _, t in ipairs(hand:effa4()) do
+        mount:lighta(t, junme * 7)
+      end
+    end
   end
-  
-  return init:step7() == 5 and init:step4() <= 5
+
+  if who == self then
+    junme = junme + 1
+    if hand:step4() <= hand:step7() and hand:step4() <= hand:step13() then
+      for _, t in ipairs(hand:effa4()) do
+        mount:lighta(t, junme * 7)
+      end
+    else
+      for _, t in ipairs(hand:effa()) do
+        mount:lighta(t, junme * 7)
+      end
+      for _, t in ipairs(hand:effa4()) do
+        mount:lighta(t, junme * -7)
+      end
+    end
+    if handr:ready() then
+      for _, t in ipairs(handr:effa()) do
+        print(t, "有危險")
+      end
+    end
+    if handc:ready() then
+      for _, t in ipairs(handc:effa()) do
+        print(t, "有危險")
+      end
+    end
+    if handl:ready() then
+      for _, t in ipairs(handl:effa()) do
+        print(t, "有危險")
+      end
+    end
+  end
 end
