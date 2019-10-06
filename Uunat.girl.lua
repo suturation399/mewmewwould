eb587575eb0c7062f7ebebd218a40fce8c6c01a7
suturation399 <33090516+suturation399@users.noreply.github.com>
2019-10-06 10:37:48 +0800

    Update Uunat.girl.lua

--- Uunat.girl.lua
+++ Uunat.girl.lua
@@ -1,5 +1,22 @@
 function onmonkey()
+  local exist = exists[self:index()]
+  
+  for i = 2, 8 do
+    exist:incmk(T34.new(i .. "p"), 55)
+    exist:incmk(T34.new(i .. "s"), 55)
+    exist:incmk(T34.new(i .. "m"), 55)
+  end
+end
+
+function checkinit()
   junme = 0
+  
+  if who ~= self or iter > 222 then
+    return true
+  end
+  
+  local ny = init:ct(T34.new("1m")) + init:ct(T34.new("9m")) + init:ct(T34.new("1p")) + init:ct(T34.new("9p")) + init:ct(T34.new("1s")) + init:ct(T34.new("9s")) + init:ct(T34.new("1f")) + init:ct(T34.new("2f")) + init:ct(T34.new("3f")) + init:ct(T34.new("4f")) + init:ct(T34.new("1y")) + init:ct(T34.new("2y")) + init:ct(T34.new("3y"))
+  return ny <= 4 and init:step7() == 5
 end
 
 function ondraw()
@@ -30,20 +47,11 @@ function ondraw()
 
   if who == self then
     junme = junme + 1
-    if hand:step4() <= hand:step7() and hand:step4() <= hand:step13() then
-      for _, t in ipairs(hand:effa4()) do
-        mount:lighta(t, junme * 14)
-      end
-      for _, t in ipairs(hand:effa()) do
-        mount:lighta(t, junme * -21)
-      end
-    else
-      for _, t in ipairs(hand:effa()) do
-        mount:lighta(t, junme * 14)
-      end
-      for _, t in ipairs(hand:effa4()) do
-        mount:lighta(t, junme * -7)
-      end
+    for _, t in ipairs(hand:effa()) do
+      mount:lighta(t, junme * 14)
+    end
+    for _, t in ipairs(hand:effa4()) do
+      mount:lighta(t, junme * 21)
     end
     if handr:step() == 0 then 
       for _, t in ipairs(handr:effa()) do
