8d64e4aa4eb094230e0d60c20392c90cc99bf902
suturation399 <33090516+suturation399@users.noreply.github.com>
2019-11-17 01:38:18 +0800

    Update Yae.girl.lua

--- Yae.girl.lua
+++ Yae.girl.lua
@@ -1,5 +1,20 @@
 function onmonkey()
   junme = 0
+  local exist = exists[self:index()]
+  
+  for i = 2, 8 do
+    exist:incmk(T34.new(i .. "p"), 45)
+    exist:incmk(T34.new(i .. "s"), 45)
+    exist:incmk(T34.new(i .. "m"), 45)
+  end
+end
+
+function checkinit()
+  if who ~= self or iter > 133 then
+    return true
+    
+  local ny = init:ct(T34.new("1m")) + init:ct(T34.new("9m")) + init:ct(T34.new("1p")) + init:ct(T34.new("9p")) + init:ct(T34.new("1s")) + init:ct(T34.new("9s")) + init:ct(T34.new("1f")) + init:ct(T34.new("2f")) + init:ct(T34.new("3f")) + init:ct(T34.new("4f")) + init:ct(T34.new("1y")) + init:ct(T34.new("2y")) + init:ct(T34.new("3y"))
+  return ny <= 2 and init:step() <= 4 and init:step() >= 5
 end
 
 function ondraw()
@@ -20,7 +35,7 @@ function ondraw()
     junme = junme + 1
     if steps >= 1 then
       for _, t in ipairs(hands:effa()) do
-        mount:lighta(t, junme * 4)
+        mount:lighta(t, junme * 5)
       end
     else
       tenjun = tenjun + 1
