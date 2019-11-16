79f2abb492f79081a46a1fb3574bd6de3fcb524e
suturation399 <33090516+suturation399@users.noreply.github.com>
2019-11-16 19:14:13 +0800

    Create Yae.girl.lua

---
+++ Yae.girl.lua
@@ -0,0 +1,38 @@
+function onmonkey()
+  junme = 0
+end
+
+function ondraw()
+  local drids = mount:getdrids()
+  local hands = game:gethand(self)
+  local steps = hands:step(self)
+  local ctx = game:getformctx(self)
+  
+  if who ~= self or rinshan then
+    return
+  end
+  
+  if who ~= self or rinshan then
+    return
+  end
+
+  if who == self then
+    junme = junme + 1
+    if steps >= 1 then
+      for _, t in ipairs(hands:effa()) do
+        mount:lighta(t, junme * 4)
+      end
+    else
+      tenjun = tenjun + 1
+      if ctx.riichi == 0 then
+        for _, t in ipairs(hands:effa()) do
+          mount:lighta(t, 66 * tenjun - 33)
+        end
+      else
+        for _, t in ipairs(hands:effa()) do
+          mount:lighta(t, -5)
+        end
+      end
+    end
+  end
+end
