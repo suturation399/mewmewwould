986dddc728173916174f62dd244301d7168eb6ee
suturation399 <33090516+suturation399@users.noreply.github.com>
2019-11-16 17:28:19 +0800

    Create kurumi.girl.lua

---
+++ kurumi.girl.lua
@@ -0,0 +1,33 @@
+function onmonkey()
+  junme = 0
+  tenjun = 0
+end
+
+function ondraw()
+  local drids = mount:getdrids()
+  local hands = game:gethand(self)
+  local steps = hands:step(self)
+  local ctx = game:getformctx(self)
+  
+  if who ~= self or rinshan then
+    return
+  end
+
+  if who == self then
+    junme = junme + 1
+    if steps >= 1 then
+      for _, t in ipairs(hands:effa()) do
+        mount:lighta(t, junme * 4)
+      end
+    else
+      tenjun = tenjun + 1
+      if ctx.riichi ~= 0 then
+        for _, t in ipairs(hands:effa()) do
+          mount:lighta(t, 66 * tenjun)
+        end
+      else
+        return
+      end
+    end
+  end
+end
