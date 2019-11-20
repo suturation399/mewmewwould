b4797ee42e909f08d2b7cbe59da04167d083cb54
suturation399 <33090516+suturation399@users.noreply.github.com>
2019-11-20 17:11:21 +0800

    Update and rename Yae.girl.lua to yae.girl.lua

--- yae.girl.lua
+++ yae.girl.lua
@@ -14,7 +14,7 @@ function checkinit()
     return true
     
   local ny = init:ct(T34.new("1m")) + init:ct(T34.new("9m")) + init:ct(T34.new("1p")) + init:ct(T34.new("9p")) + init:ct(T34.new("1s")) + init:ct(T34.new("9s")) + init:ct(T34.new("1f")) + init:ct(T34.new("2f")) + init:ct(T34.new("3f")) + init:ct(T34.new("4f")) + init:ct(T34.new("1y")) + init:ct(T34.new("2y")) + init:ct(T34.new("3y"))
-  return ny <= 2 and init:step() <= 4 and init:step() >= 5
+  return ny <= 2 and init:step() <= 4 and init:step7() == 5
 end
 
 function ondraw()
@@ -23,14 +23,14 @@ function ondraw()
   local steps = hands:step(self)
   local ctx = game:getformctx(self)
   
-  if who ~= self or rinshan then
+  if rinshan then
     return
   end
   
-  if who ~= self or rinshan then
-    return
+  if who ~= self then
+    
   end
-
+    
   if who == self then
     junme = junme + 1
     if steps >= 1 then
