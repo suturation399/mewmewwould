f7a9bf1402aba5525293fe8a04609cf019487b4e
suturation399 <33090516+suturation399@users.noreply.github.com>
2019-11-20 17:40:06 +0800

    Update yae.girl.lua

--- yae.girl.lua
+++ yae.girl.lua
@@ -12,15 +12,18 @@ end
 function checkinit()
   if who ~= self or iter > 133 then
     return true
+  end
     
   local ny = init:ct(T34.new("1m")) + init:ct(T34.new("9m")) + init:ct(T34.new("1p")) + init:ct(T34.new("9p")) + init:ct(T34.new("1s")) + init:ct(T34.new("9s")) + init:ct(T34.new("1f")) + init:ct(T34.new("2f")) + init:ct(T34.new("3f")) + init:ct(T34.new("4f")) + init:ct(T34.new("1y")) + init:ct(T34.new("2y")) + init:ct(T34.new("3y"))
-  return ny <= 2 and init:step() <= 4 and init:step7() == 5
+  return ny <= 3 and init:step() <= 4 and init:step7() == 5
 end
 
 function ondraw()
   local drids = mount:getdrids()
+  local hand = game:gethand(self)
   local hands = game:gethand(self)
   local steps = hands:step(self)
+  local step = hand:step(self)
   local ctx = game:getformctx(self)
   
   if rinshan then
@@ -28,26 +31,25 @@ function ondraw()
   end
   
   if who ~= self then
-    
+    if ctx.riichi == 0 then
+      for _, t in ipairs(hands:effa()) do
+        mount:lighta(t, junme * 5 * (6 - step))
+      end
+      for _, t in ipairs(hand:effa()) do
+        mount:lighta(t, junme * 5 * step)
+      end
+    else
+      return
+    end
   end
     
   if who == self then
     junme = junme + 1
-    if steps >= 1 then
-      for _, t in ipairs(hands:effa()) do
-        mount:lighta(t, junme * 5)
-      end
-    else
-      tenjun = tenjun + 1
-      if ctx.riichi == 0 then
-        for _, t in ipairs(hands:effa()) do
-          mount:lighta(t, 66 * tenjun - 33)
-        end
-      else
-        for _, t in ipairs(hands:effa()) do
-          mount:lighta(t, -5)
-        end
-      end
+    for _, t in ipairs(hands:effa()) do
+      mount:lighta(t, junme * -5 * 2)
+    end
+    for _, t in ipairs(hands:effa4()) do
+      mount:lighta(t, junme * 5 * 3)
     end
   end
 end
