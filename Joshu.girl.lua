716e831551bb771efafd670329f6a4397fd1b47d
suturation399 <33090516+suturation399@users.noreply.github.com>
2019-12-04 13:37:19 +0800

    Update Joshu.girl.lua

--- Joshu.girl.lua
+++ Joshu.girl.lua
@@ -9,6 +9,7 @@ function ondice()
 end
 
 function onmonkey()
+  print("泰坦之鎖")
   cutm = dogem
   cutp = dogep
   cuts = doges
@@ -19,42 +20,49 @@ function onmonkey()
   local cutmk = -472
   
   if cutm <= 1 then
+    print("2m")
     existself:incmk(T34.new("2m"), cutmk)
     existl:incmk(T34.new("2m"), cutmk)
     existc:incmk(T34.new("2m"), cutmk)
     existr:incmk(T34.new("2m"), cutmk)
   end
   if cutm == 2 then
+    print("3m")
     existself:incmk(T34.new("3m"), cutmk)
     existl:incmk(T34.new("3m"), cutmk)
     existc:incmk(T34.new("3m"), cutmk)
     existr:incmk(T34.new("3m"), cutmk)
   end
   if cutm == 3 then
+    print("4m")
     existself:incmk(T34.new("4m"), cutmk)
     existl:incmk(T34.new("4m"), cutmk)
     existc:incmk(T34.new("4m"), cutmk)
     existr:incmk(T34.new("4m"), cutmk)
   end
   if cutm == 4 then
+    print("5m")
     existself:incmk(T34.new("5m"), cutmk)
     existl:incmk(T34.new("5m"), cutmk)
     existc:incmk(T34.new("5m"), cutmk)
     existr:incmk(T34.new("5m"), cutmk)
   end
   if cutm == 5 then
+    print("6m")
     existself:incmk(T34.new("6m"), cutmk)
     existl:incmk(T34.new("6m"), cutmk)
     existc:incmk(T34.new("6m"), cutmk)
     existr:incmk(T34.new("6m"), cutmk)
   end
   if cutm == 6 then
+    print("7m")
     existself:incmk(T34.new("7m"), cutmk)
     existl:incmk(T34.new("7m"), cutmk)
     existc:incmk(T34.new("7m"), cutmk)
     existr:incmk(T34.new("7m"), cutmk)
   end
   if cutm >= 7 then
+    print("8m")
     existself:incmk(T34.new("8m"), cutmk)
     existl:incmk(T34.new("8m"), cutmk)
     existc:incmk(T34.new("8m"), cutmk)
@@ -62,42 +70,49 @@ function onmonkey()
   end
   
   if cutp <= 1 then
+    print("2p")
     existself:incmk(T34.new("2p"), cutmk)
     existl:incmk(T34.new("2p"), cutmk)
     existc:incmk(T34.new("2p"), cutmk)
     existr:incmk(T34.new("2p"), cutmk)
   end
   if cutp == 2 then
+    print("3p")
     existself:incmk(T34.new("3p"), cutmk)
     existl:incmk(T34.new("3p"), cutmk)
     existc:incmk(T34.new("3p"), cutmk)
     existr:incmk(T34.new("3p"), cutmk)
   end
   if cutp == 3 then
+    print("4p")
     existself:incmk(T34.new("4p"), cutmk)
     existl:incmk(T34.new("4p"), cutmk)
     existc:incmk(T34.new("4p"), cutmk)
     existr:incmk(T34.new("4p"), cutmk)
   end
   if cutp == 4 then
+    print("5p")
     existself:incmk(T34.new("5p"), cutmk)
     existl:incmk(T34.new("5p"), cutmk)
     existc:incmk(T34.new("5p"), cutmk)
     existr:incmk(T34.new("5p"), cutmk)
   end
   if cutp == 5 then
+    print("6p")
     existself:incmk(T34.new("6p"), cutmk)
     existl:incmk(T34.new("6p"), cutmk)
     existc:incmk(T34.new("6p"), cutmk)
     existr:incmk(T34.new("6p"), cutmk)
   end
   if cutp == 6 then
+    print("7p")
     existself:incmk(T34.new("7p"), cutmk)
     existl:incmk(T34.new("7p"), cutmk)
     existc:incmk(T34.new("7p"), cutmk)
     existr:incmk(T34.new("7p"), cutmk)
   end
   if cutp >= 7 then
+    print("8p")
     existself:incmk(T34.new("8p"), cutmk)
     existl:incmk(T34.new("8p"), cutmk)
     existc:incmk(T34.new("8p"), cutmk)
@@ -105,42 +120,49 @@ function onmonkey()
   end
   
   if cuts <= 1 then
+    print("2s")
     existself:incmk(T34.new("2s"), cutmk)
     existl:incmk(T34.new("2s"), cutmk)
     existc:incmk(T34.new("2s"), cutmk)
     existr:incmk(T34.new("2s"), cutmk)
   end
   if cuts == 2 then
+    print("3s")
     existself:incmk(T34.new("3s"), cutmk)
     existl:incmk(T34.new("3s"), cutmk)
     existc:incmk(T34.new("3s"), cutmk)
     existr:incmk(T34.new("3s"), cutmk)
   end
   if cuts == 3 then
+    print("4s")
     existself:incmk(T34.new("4s"), cutmk)
     existl:incmk(T34.new("4s"), cutmk)
     existc:incmk(T34.new("4s"), cutmk)
     existr:incmk(T34.new("4s"), cutmk)
   end
   if cuts == 4 then
+    print("5s")
     existself:incmk(T34.new("5s"), cutmk)
     existl:incmk(T34.new("5s"), cutmk)
     existc:incmk(T34.new("5s"), cutmk)
     existr:incmk(T34.new("5s"), cutmk)
   end
   if cuts == 5 then
+    print("6s")
     existself:incmk(T34.new("6s"), cutmk)
     existl:incmk(T34.new("6s"), cutmk)
     existc:incmk(T34.new("6s"), cutmk)
     existr:incmk(T34.new("6s"), cutmk)
   end
   if cuts == 6 then
+    print("7s")
     existself:incmk(T34.new("7s"), cutmk)
     existl:incmk(T34.new("7s"), cutmk)
     existc:incmk(T34.new("7s"), cutmk)
     existr:incmk(T34.new("7s"), cutmk)
   end
   if cuts >= 7 then
+    print("8s")
     existself:incmk(T34.new("8s"), cutmk)
     existl:incmk(T34.new("8s"), cutmk)
     existc:incmk(T34.new("8s"), cutmk)
@@ -172,7 +194,7 @@ function ondraw()
   local handl = game:gethand(self:left())
   
   if mount:remainpii() == 70 then
-    if cutm == 1 then
+    if cutm <= 1 then
       mount:loadb(T37.new("2m"), 4)
     end
     if cutm == 2 then
@@ -190,10 +212,10 @@ function ondraw()
     if cutm == 6 then
       mount:loadb(T37.new("7m"), 4)
     end
-    if cutm == 7 then
+    if cutm >= 7 then
       mount:loadb(T37.new("8m"), 4)
     end
-    if cutp == 1 then
+    if cutp <= 1 then
       mount:loadb(T37.new("2p"), 4)
     end
     if cutp == 2 then
@@ -211,10 +233,10 @@ function ondraw()
     if cutp == 6 then
       mount:loadb(T37.new("7p"), 4)
     end
-    if cutp == 7 then
+    if cutp >= 7 then
       mount:loadb(T37.new("8p"), 4)
     end
-    if cuts == 1 then
+    if cuts <= 1 then
       mount:loadb(T37.new("2s"), 4)
     end
     if cuts == 2 then
@@ -232,7 +254,7 @@ function ondraw()
     if cuts == 6 then
       mount:loadb(T37.new("7s"), 4)
     end
-    if cuts == 7 then
+    if cuts >= 7 then
       mount:loadb(T37.new("8s"), 4)
     end
   end
@@ -271,7 +293,8 @@ function ondraw()
         end
       else
         for _, t in ipairs(handr:effa()) do
-          mount:lighta(t, 472)
+          mount:loadb(t, 1)
+          mount:lightb(t, 472)
           print(t, "有危險")
         end
       end
@@ -284,7 +307,8 @@ function ondraw()
         end
       else
         for _, t in ipairs(handc:effa()) do
-          mount:lighta(t, 472)
+          mount:loadb(t, 1)
+          mount:lightb(t, 472)
           print(t, "有危險")
         end
       end
@@ -297,7 +321,8 @@ function ondraw()
         end
       else
         for _, t in ipairs(handl:effa()) do
-          mount:lighta(t, 472)
+          mount:loadb(t, 1)
+          mount:lightb(t, 472)
           print(t, "有危險")
         end
       end
