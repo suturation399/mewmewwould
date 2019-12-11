265458994efbb900f3589f0d9f884eecefa8df16
suturation399 <33090516+suturation399@users.noreply.github.com>
2019-12-11 22:36:26 +0800

    Update Joshu.girl.lua

--- Joshu.girl.lua
+++ Joshu.girl.lua
@@ -22,7 +22,7 @@ function ondraw()
   if who ~= self then
     if hand:ready() then
       for _, t in ipairs(hand:effa()) do
-        if mount:remaina(t) >= 1
+        if mount:remaina(t) >= 1 then
           mount:lighta(t, -100000)
         end
       end
@@ -38,7 +38,7 @@ function ondraw()
     if handr:ready() then
       for _, t in ipairs(handr:effa()) do
         print(t, "有危險")
-        if not hands:ready() and mount:remaina(t) >= 1 then
+        if (not hands:step() ~= 0 and mount:remaina(t) >= 1) then
            mount:lighta(t, 100000)
         else
           mount:lighta(t, -100)
@@ -48,7 +48,7 @@ function ondraw()
     if handc:ready() then
       for _, t in ipairs(handc:effa()) do
         print(t, "有危險")
-        if not hands:ready() and mount:remaina(t) >= 1 then
+        if (not hands:step() ~= 0 and mount:remaina(t) >= 1) then
            mount:lighta(t, 100000)
         else
           mount:lighta(t, -100)
@@ -58,7 +58,7 @@ function ondraw()
     if handl:ready() then
       for _, t in ipairs(handl:effa()) do
         print(t, "有危險")
-        if not hands:ready() and mount:remaina(t) >= 1 then
+        if (not hands:step() ~= 0 and mount:remaina(t) >= 1) then
            mount:lighta(t, 100000)
         else
           mount:lighta(t, -100)
