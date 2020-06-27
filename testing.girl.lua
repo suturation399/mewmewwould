1c8206337ddd39d9bcd75cf68f5e44507ed34870
suturation399 <33090516+suturation399@users.noreply.github.com>
2020-06-28 00:48:05 +0800

    Update testing.girl.lua

--- testing.girl.lua
+++ testing.girl.lua
@@ -1,84 +1,35 @@
-function onmonkey()
-  junme = 0
-end
-
-function checkinit()
-  if who ~= self or iter > 3 then
-    return true
-  end
-  
-  return init:step() <= 4
-end
-
 function ondraw()
   local hands = game:gethand(self)
-  local effas = hands:effa()
-  local steps = hands:step(self)
   local handr = game:gethand(self:right())
   local handc = game:gethand(self:cross())
   local handl = game:gethand(self:left())
-  local ctx = game:getformctx(self)
-  local rule = game:getrule()
-  local drids = mount:getdrids()
+  local hand = game:gethand()
+  local effas = hands:effa()
+  local steps = hands:step(self)
+  local sw = game:getselfwind(self)
+  local rw = game:getroundwind()
   
-  if who ~= self or rinshan then
-    return
+  if who ~= self then
+    return 
   end
   
   if who == self then
-    junme = junme + 1
     for _, t in ipairs(effas) do
-      mount:lighta(t, junme * 3)
-    end
-    if steps <= 1 then
-      for _, t in ipairs(effas) do
-        local ntenpai = mount:remaina(t)
-        print(t, "預計牌山殘枚數為", ntenpai)
-      end
-    end
-    if junme >= 3 then
-      print("預計下家向聽數為", handr:step())
-      print("預計對家向聽數為", handc:step())
-      print("預計上家向聽數為", handl:step())
-    end
-    if junme >= 6 then
-      if handr:ready() then
-        for _, t in ipairs(handr:effa()) do
-          local ntenpair = mount:remaina(t)
-          print("預計危險牌為", t, "預計摸牌前牌山殘枚數為", ntenpair)
-          if steps ~= 0 then
-            mount:lighta(t, 307)
-            mount:lightb(t, 307)
-          else
-            mount:lighta(t, -31)
-            mount:lightb(t, -31)
-          end
+      local neffas = mount:remaina(t)
+      if neffas <= 0 then
+        mount:lighta(t, 0)
+      else
+        if neffas == 1 then
+          mount:lighta(t, 307)
         end
-      end
-      if handc:ready() then
-        for _, t in ipairs(handc:effa()) do
-          local ntenpaic = mount:remaina(t)
-          print("預計危險牌為", t, "預計摸牌前牌山殘枚數為", ntenpaic)
-          if steps ~= 0 then
-            mount:lighta(t, 307)
-            mount:lightb(t, 307)
-          else
-            mount:lighta(t, -31)
-            mount:lightb(t, -31)
-          end
+        if neffas == 2 then
+          mount:lighta(t, 73)
         end
-      end
-      if handl:ready() then
-        for _, t in ipairs(handl:effa()) do
-          local ntenpail = mount:remaina(t)
-          print("預計危險牌為", t, "預計摸牌前牌山殘枚數為", ntenpail)
-          if steps ~= 0 then
-            mount:lighta(t, 307)
-            mount:lightb(t, 307)
-          else
-            mount:lighta(t, -31)
-            mount:lightb(t, -31)
-          end
+        if neffas == 3 then
+          mount:lighta(t, 17)
+        end
+        if neffas >= 4 then
+          mount:lighta(t, 4)
         end
       end
     end
