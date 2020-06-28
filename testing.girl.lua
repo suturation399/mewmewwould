27ef89235bbb8227293146ff5313420a9b62ffea
suturation399 <33090516+suturation399@users.noreply.github.com>
2020-06-29 01:14:13 +0800

    Update testing.girl.lua

--- testing.girl.lua
+++ testing.girl.lua
@@ -1,37 +1,36 @@
+function onmonkey()
+  junme = 0
+end
+
+function checkinit()
+  if who ~= self or iter > 3 then
+    return true
+  end
+  
+  return init:step() <= 4
+end
+
 function ondraw()
   local hands = game:gethand(self)
+  local effas = hands:effa()
+  local steps = hands:step(self)
   local handr = game:gethand(self:right())
   local handc = game:gethand(self:cross())
   local handl = game:gethand(self:left())
-  local hand = game:gethand()
-  local effas = hands:effa()
-  local steps = hands:step(self)
-  local sw = game:getselfwind(self)
-  local rw = game:getroundwind()
+  local ctx = game:getformctx(self)
+  local rule = game:getrule()
+  local drids = mount:getdrids()
   
-  if who ~= self then
-    return 
+  if who ~= self or rinshan then
+    return
   end
   
   if who == self then
+    junme = junme + 1
     for _, t in ipairs(effas) do
       local neffas = mount:remaina(t)
-      if neffas <= 0 then
-        mount:lighta(t, 0)
-      else
-        if neffas == 1 then
-          mount:lighta(t, 307)
-        end
-        if neffas == 2 then
-          mount:lighta(t, 73)
-        end
-        if neffas == 3 then
-          mount:lighta(t, 17)
-        end
-        if neffas >= 4 then
-          mount:lighta(t, 4)
-        end
-      end
+      local ne = neffas * -1 + 8
+      mount:lighta(t, ne * 30)
     end
   end
 end
