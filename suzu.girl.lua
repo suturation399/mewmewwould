80269e7f898eed4ccba36f2ce34677d9da63c472
suturation399 <33090516+suturation399@users.noreply.github.com>
2020-08-02 17:46:21 +0800

    Create suzu.girl.lua

---
+++ suzu.girl.lua
@@ -0,0 +1,17 @@
+doge = 0
+bakuhatsu = 0
+
+function ondice()
+  doge = rand:gen(50)
+  
+  if doge >= 40 then
+    bakuhatsu = bakuhatsu + 1
+  end
+end
+
+function onmonkey()
+  status = doge
+  
+  if bakuhatsu = 0 then
+    
+  
