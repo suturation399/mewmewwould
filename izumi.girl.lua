0fdbff65a54b4d1d02d5a662caf9b3e8a1fd1371
suturation399 <33090516+suturation399@users.noreply.github.com>
2020-08-29 01:38:27 +0800

    Update izumi.girl.lua

--- izumi.girl.lua
+++ izumi.girl.lua
@@ -1,172 +1,58 @@
-doge = 0
-
-function ondice()
-  doge = rand:gen(24)
-end
-
-function onmonkey()
-  status = doge
-  local sw = game:getselfwind(self)
-  local rw = game:getroundwind()
-  local exist = exists[self:index()]
-  
-  if status <= 1 then
-    exist:incmk(T34.new("1y"), 144)
-    exist:incmk(T34.new("2y"), -15)
-    exist:incmk(T34.new("3y"), -15)
-    for i = 1, 4 do
-      exist:incmk(T34.new(i .. "f"), -15)
-    end
-  end
-  
-  if status == 2 then
-    exist:incmk(T34.new("2y"), 144)
-    exist:incmk(T34.new("1y"), -15)
-    exist:incmk(T34.new("3y"), -15)
-    for i = 1, 4 do
-      exist:incmk(T34.new(i .. "f"), -15)
-    end
-  end
-
-  if status == 3 then
-    exist:incmk(T34.new("3y"), 144)
-    exist:incmk(T34.new("1y"), -15)
-    exist:incmk(T34.new("2y"), -15)
-    for i = 1, 4 do
-      exist:incmk(T34.new(i .. "f"), -15)
-    end
-  end
-
-  if status == 4 then
-    if sw == 1 then
-      exist:incmk(T34.new("1f"), 144)
-      exist:incmk(T34.new("2f"), -15)
-      exist:incmk(T34.new("3f"), -15)
-      exist:incmk(T34.new("4f"), -15)
-      for i = 1, 3 do
-        exist:incmk(T34.new(i .. "y"), -15)
-      end
-    end
-    if sw == 2 then
-      exist:incmk(T34.new("2f"), 144)
-      exist:incmk(T34.new("1f"), -15)
-      exist:incmk(T34.new("3f"), -15)
-      exist:incmk(T34.new("4f"), -15)
-      for i = 1, 3 do
-        exist:incmk(T34.new(i .. "y"), -15)
-      end
-    end
-    if sw == 3 then
-      exist:incmk(T34.new("3f"), 144)
-      exist:incmk(T34.new("2f"), -15)
-      exist:incmk(T34.new("1f"), -15)
-      exist:incmk(T34.new("4f"), -15)
-      for i = 1, 3 do
-        exist:incmk(T34.new(i .. "y"), -15)
-      end
-    end
-    if sw == 4 then
-      exist:incmk(T34.new("4f"), 144)
-      exist:incmk(T34.new("2f"), -15)
-      exist:incmk(T34.new("3f"), -15)
-      exist:incmk(T34.new("1f"), -15)
-      for i = 1, 3 do
-        exist:incmk(T34.new(i .. "y"), -15)
-      end
-    end
-  end
-  
-  if status == 5 then
-    if rw == 1 then
-      exist:incmk(T34.new("1f"), 144)
-      exist:incmk(T34.new("2f"), -15)
-      exist:incmk(T34.new("3f"), -15)
-      exist:incmk(T34.new("4f"), -15)
-      for i = 1, 3 do
-        exist:incmk(T34.new(i .. "y"), -15)
-      end
-    end
-    if rw == 2 then
-      exist:incmk(T34.new("2f"), 144)
-      exist:incmk(T34.new("1f"), -15)
-      exist:incmk(T34.new("3f"), -15)
-      exist:incmk(T34.new("4f"), -15)
-      for i = 1, 3 do
-        exist:incmk(T34.new(i .. "y"), -15)
-      end
-    end
-    if rw >= 3 then
-      exist:incmk(T34.new("3f"), 144)
-      exist:incmk(T34.new("2f"), -15)
-      exist:incmk(T34.new("1f"), -15)
-      exist:incmk(T34.new("4f"), -15)
-      for i = 1, 3 do
-        exist:incmk(T34.new(i .. "y"), -15)
-      end
-    end
-  end
-  
-  if status >= 6 then
-    for i = 2, 8 do
-      exist:incmk(T34.new(i .. "p"), 45)
-      exist:incmk(T34.new(i .. "s"), 45)
-      exist:incmk(T34.new(i .. "m"), 45)
-    end
-  end
-end
+ok = 0
 
 function checkinit()
   junme = 0
   local sw = game:getselfwind(self)
   local rw = game:getroundwind()
+  ok = 0
   
   if who ~= self or iter > 87 then
     return true
   end
-
-  if status <= 1 then
-    return init:step() >= 3 and init:step() <= 4 and init:step7() >= 4 and init:ct(T34.new("1y")) == 2
+  
+  if init:ct(T34.new("1y")) >= 2 and init:step4() >= 3 and init:step4() <= 4 then
+    ok = 1
   end
-
-  if status == 2 then
-    return init:step() >= 3 and init:step() <= 4 and init:step7() >= 4 and init:ct(T34.new("2y")) == 2
+  
+  if init:ct(T34.new("2y")) >= 2 and init:step4() >= 3 and init:step4() <= 4 then
+    ok = 1
   end
-
-  if status == 3 then
-    return init:step() >= 3 and init:step() <= 4 and init:step7() >= 4 and init:ct(T34.new("3y")) == 2
+    
+  if init:ct(T34.new("3y")) >= 2 and init:step4() >= 3 and init:step4() <= 4 then
+    ok = 1
   end
-
-  if status == 4 then
-    if sw == 1 then
-      return init:step() >= 3 and init:step() <= 4 and init:step7() >= 4 and init:ct(T34.new("1f")) == 2
-    end
-    if sw == 2 then
-      return init:step() >= 3 and init:step() <= 4 and init:step7() >= 4 and init:ct(T34.new("2f")) == 2
-    end
-    if sw == 3 then
-      return init:step() >= 3 and init:step() <= 4 and init:step7() >= 4 and init:ct(T34.new("3f")) == 2
-    end
-    if sw == 4 then
-      return init:step() >= 3 and init:step() <= 4 and init:step7() >= 4 and init:ct(T34.new("4f")) == 2
+  
+  if rw == 1 or sw == 1 then
+    if init:ct(T34.new("1f")) >= 2 and init:step4() >= 3 and init:step4() <= 4 then
+      ok = 1
     end
   end
-
-  if status == 5 then
-    if rw == 1 then
-      return init:step() >= 3 and init:step() <= 4 and init:step7() >= 4 and init:ct(T34.new("1f")) == 2
-    end
-    if rw == 2 then
-      return init:step() >= 3 and init:step() <= 4 and init:step7() >= 4 and init:ct(T34.new("2f")) == 2
-    end
-    if rw == 3 then
-      return init:step() >= 3 and init:step() <= 4 and init:step7() >= 4 and init:ct(T34.new("3f")) == 2
+  
+  if rw == 2 or sw == 2 then
+    if init:ct(T34.new("2f")) >= 2 and init:step4() >= 3 and init:step4() <= 4 then
+      ok = 1
     end
   end
-
-  if status >= 6 then
+  
+  if rw == 3 or sw == 3 then
+    if init:ct(T34.new("3f")) >= 2 and init:step4() >= 3 and init:step4() <= 4 then
+      ok = 1
+    end
+  end
+  
+  if sw == 4 then
+    if init:ct(T34.new("4f")) >= 2 and init:step4() >= 3 and init:step4() <= 4 then
+      ok = 1
+    end
+  end
+  
   local ny = init:ct(T34.new("1m")) + init:ct(T34.new("9m")) + init:ct(T34.new("1p")) + init:ct(T34.new("9p")) + init:ct(T34.new("1s")) + init:ct(T34.new("9s")) + init:ct(T34.new("1f")) + init:ct(T34.new("2f")) + init:ct(T34.new("3f")) + init:ct(T34.new("4f")) + init:ct(T34.new("1y")) + init:ct(T34.new("2y")) + init:ct(T34.new("3y"))
-    return ny <= 3 and init:step() >= 3 and init:step() <= 4
+  
+  if ny <= 3 and init:step() >= 3 and init:step() <= 4 then
+    ok = 1
   end
+  
+  return ok >= 1
 end
 
 function ondraw()
