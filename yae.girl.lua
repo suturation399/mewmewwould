e34ec948b0e1e509a8ceb1c0fe8641c42dac2d23
suturation399 <33090516+suturation399@users.noreply.github.com>
2020-09-28 16:04:01 +0800

    Update yae.girl.lua

--- yae.girl.lua
+++ yae.girl.lua
@@ -1,189 +1,23 @@
-doge = 0
-sp = 90
-
-function ondice()
-  local extra = game:getextraround()
-
-  if extra == 0 then
-    sp = 90
-  end
-  if extra == 1 then
-    sp = 60
-  end
-  if extra >= 2 then
-    sp = 30
-  end
   
-  doge = rand:gen(sp)
-end
-
-function onmonkey()
-  status = doge
-  local sw = game:getselfwind(self)
-  local rw = game:getroundwind()
-  local exist = exists[self:index()]
-  
-  if status <= 1 then
-    exist:incmk(T34.new("1y"), 144)
-    exist:incmk(T34.new("2y"), -15)
-    exist:incmk(T34.new("3y"), -15)
-    for i = 1, 4 do
-      exist:incmk(T34.new(i .. "f"), -15)
-    end
-  end
-  
-  if status == 2 then
-    exist:incmk(T34.new("2y"), 144)
-    exist:incmk(T34.new("1y"), -15)
-    exist:incmk(T34.new("3y"), -15)
-    for i = 1, 4 do
-      exist:incmk(T34.new(i .. "f"), -15)
-    end
-  end
-
-  if status == 3 then
-    exist:incmk(T34.new("3y"), 144)
-    exist:incmk(T34.new("1y"), -15)
-    exist:incmk(T34.new("2y"), -15)
-    for i = 1, 4 do
-      exist:incmk(T34.new(i .. "f"), -15)
-    end
-  end
-
-  if status == 4 then
-    if sw == 1 then
-      exist:incmk(T34.new("1f"), 144)
-      exist:incmk(T34.new("2f"), -15)
-      exist:incmk(T34.new("3f"), -15)
-      exist:incmk(T34.new("4f"), -15)
-      for i = 1, 3 do
-        exist:incmk(T34.new(i .. "y"), -15)
-      end
-    end
-    if sw == 2 then
-      exist:incmk(T34.new("2f"), 144)
-      exist:incmk(T34.new("1f"), -15)
-      exist:incmk(T34.new("3f"), -15)
-      exist:incmk(T34.new("4f"), -15)
-      for i = 1, 3 do
-        exist:incmk(T34.new(i .. "y"), -15)
-      end
-    end
-    if sw == 3 then
-      exist:incmk(T34.new("3f"), 144)
-      exist:incmk(T34.new("2f"), -15)
-      exist:incmk(T34.new("1f"), -15)
-      exist:incmk(T34.new("4f"), -15)
-      for i = 1, 3 do
-        exist:incmk(T34.new(i .. "y"), -15)
-      end
-    end
-    if sw == 4 then
-      exist:incmk(T34.new("4f"), 144)
-      exist:incmk(T34.new("2f"), -15)
-      exist:incmk(T34.new("3f"), -15)
-      exist:incmk(T34.new("1f"), -15)
-      for i = 1, 3 do
-        exist:incmk(T34.new(i .. "y"), -15)
-      end
-    end
-  end
-  
-  if status == 5 then
-    if rw == 1 then
-      exist:incmk(T34.new("1f"), 144)
-      exist:incmk(T34.new("2f"), -15)
-      exist:incmk(T34.new("3f"), -15)
-      exist:incmk(T34.new("4f"), -15)
-      for i = 1, 3 do
-        exist:incmk(T34.new(i .. "y"), -15)
-      end
-    end
-    if rw == 2 then
-      exist:incmk(T34.new("2f"), 144)
-      exist:incmk(T34.new("1f"), -15)
-      exist:incmk(T34.new("3f"), -15)
-      exist:incmk(T34.new("4f"), -15)
-      for i = 1, 3 do
-        exist:incmk(T34.new(i .. "y"), -15)
-      end
-    end
-    if rw >= 3 then
-      exist:incmk(T34.new("3f"), 144)
-      exist:incmk(T34.new("2f"), -15)
-      exist:incmk(T34.new("1f"), -15)
-      exist:incmk(T34.new("4f"), -15)
-      for i = 1, 3 do
-        exist:incmk(T34.new(i .. "y"), -15)
-      end
-    end
-  end
-  
-  if status >= 6 then
-    for i = 2, 8 do
-      exist:incmk(T34.new(i .. "p"), 45)
-      exist:incmk(T34.new(i .. "s"), 45)
-      exist:incmk(T34.new(i .. "m"), 45)
-    end
-  end
-end
+ok = 0
 
 function checkinit()
   junme = 0
   local sw = game:getselfwind(self)
   local rw = game:getroundwind()
+  ok = 0
   
-  if iter > 99 then
+  if who ~= self or iter > 87 then
     return true
   end
 
-  if who ~= self then
-    return true
-  end
-  
-  if status <= 1 then
-    return init:step4() <= 3 and init:step7() == 4 and init:ct(T34.new("1y")) == 2
-  end
-
-  if status == 2 then
-    return init:step4() <= 3 and init:step7() == 4 and init:ct(T34.new("2y")) == 2
-  end
-
-  if status == 3 then
-    return init:step4() <= 3 and init:step7() == 4 and init:ct(T34.new("3y")) == 2
-  end
-
-  if status == 4 then
-    if sw == 1 then
-      return init:step4() <= 3 and init:step7() == 4 and init:ct(T34.new("1f")) == 2
-    end
-    if sw == 2 then
-      return init:step4() <= 3 and init:step7() == 4 and init:ct(T34.new("2f")) == 2
-    end
-    if sw == 3 then
-      return init:step4() <= 3 and init:step7() == 4 and init:ct(T34.new("3f")) == 2
-    end
-    if sw == 4 then
-      return init:step4() <= 3 and init:step7() == 4 and init:ct(T34.new("4f")) == 2
-    end
-  end
-
-  if status == 5 then
-    if rw == 1 then
-      return init:step4() <= 3 and init:step7() == 4 and init:ct(T34.new("1f")) == 2
-    end
-    if rw == 2 then
-      return init:step4() <= 3 and init:step7() == 4 and init:ct(T34.new("2f")) == 2
-    end
-    if rw == 3 then
-      return init:step4() <= 3 and init:step7() == 4 and init:ct(T34.new("3f")) == 2
-    end
-  end
-
-  if status >= 6 then
   local ny = init:ct(T34.new("1m")) + init:ct(T34.new("9m")) + init:ct(T34.new("1p")) + init:ct(T34.new("9p")) + init:ct(T34.new("1s")) + init:ct(T34.new("9s")) + init:ct(T34.new("1f")) + init:ct(T34.new("2f")) + init:ct(T34.new("3f")) + init:ct(T34.new("4f")) + init:ct(T34.new("1y")) + init:ct(T34.new("2y")) + init:ct(T34.new("3y"))
-    return ny <= 3 and init:step4() <= 4 and init:step7() == 5
+  
+  if ny <= 3 and init:step() <= 4 then
+    ok = 1
   end
+  
+  return ok >= 1
 end
 
 function ondraw()
@@ -204,9 +38,9 @@ function ondraw()
   end
   
   if who == self then
-    if hands:step() == ctx.riichi then
+    if hands:step() == ctx.riichi and not hands:ismenzen() then
       for _, t in ipairs(hands:effa4()) do
-        mount:lighta(t, -5)
+        mount:lighta(t, -9 * junme)
       end
     else
       for _, t in ipairs(hands:effa4()) do
@@ -238,7 +72,7 @@ function ryou (mount, game, who)
     return
   end
 
-  local mk = 100
+  local mk = 134 - junme * 6
   local hand = game:gethand(self)
   local closed = hand:closed()
   local dream = Hand.new(hand)
