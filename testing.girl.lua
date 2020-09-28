b82b16dcba642610827722105de0c4777e14ae29
suturation399 <33090516+suturation399@users.noreply.github.com>
2020-09-28 15:53:16 +0800

    Update testing.girl.lua

--- testing.girl.lua
+++ testing.girl.lua
@@ -1,198 +1,341 @@
 doge = 0
-bakuhatsu = 0
+sp = 54
 
 function ondice()
-  doge = rand:gen(50)
-  
-  if bakuhatsu ~= 0 then
-    bakuhatsu = bakuhatsu + 1
+  local extra = game:getextraround()
+
+  if extra == 0 then
+    sp = 33
+  end
+  if extra == 1 then
+    sp = 15
+  end
+  if extra >= 2 then
+    sp = 9
   end
   
-  if doge >= 45 then
-    bakuhatsu = bakuhatsu + 1
-  end
+  doge = rand:gen(sp)
 end
 
 function onmonkey()
-  junme = 0
+  status = doge
+  local sw = game:getselfwind(self)
+  local rw = game:getroundwind()
+  local exist = exists[self:index()]
   
-  if bakuhatsu == 1 then
-    print("......")
+  if status <= 1 then
+    exist:incmk(T34.new("1y"), 144)
+    exist:incmk(T34.new("2y"), -15)
+    exist:incmk(T34.new("3y"), -15)
+    for i = 1, 4 do
+      exist:incmk(T34.new(i .. "f"), -15)
+    end
+  end
+  
+  if status == 2 then
+    exist:incmk(T34.new("2y"), 144)
+    exist:incmk(T34.new("1y"), -15)
+    exist:incmk(T34.new("3y"), -15)
+    for i = 1, 4 do
+      exist:incmk(T34.new(i .. "f"), -15)
+    end
+  end
+
+  if status == 3 then
+    exist:incmk(T34.new("3y"), 144)
+    exist:incmk(T34.new("1y"), -15)
+    exist:incmk(T34.new("2y"), -15)
+    for i = 1, 4 do
+      exist:incmk(T34.new(i .. "f"), -15)
+    end
+  end
+
+  if status == 4 then
+    if sw == 1 then
+      exist:incmk(T34.new("1f"), 144)
+      exist:incmk(T34.new("2f"), -15)
+      exist:incmk(T34.new("3f"), -15)
+      exist:incmk(T34.new("4f"), -15)
+      for i = 1, 3 do
+        exist:incmk(T34.new(i .. "y"), -15)
+      end
+    end
+    if sw == 2 then
+      exist:incmk(T34.new("2f"), 144)
+      exist:incmk(T34.new("1f"), -15)
+      exist:incmk(T34.new("3f"), -15)
+      exist:incmk(T34.new("4f"), -15)
+      for i = 1, 3 do
+        exist:incmk(T34.new(i .. "y"), -15)
+      end
+    end
+    if sw == 3 then
+      exist:incmk(T34.new("3f"), 144)
+      exist:incmk(T34.new("2f"), -15)
+      exist:incmk(T34.new("1f"), -15)
+      exist:incmk(T34.new("4f"), -15)
+      for i = 1, 3 do
+        exist:incmk(T34.new(i .. "y"), -15)
+      end
+    end
+    if sw == 4 then
+      exist:incmk(T34.new("4f"), 144)
+      exist:incmk(T34.new("2f"), -15)
+      exist:incmk(T34.new("3f"), -15)
+      exist:incmk(T34.new("1f"), -15)
+      for i = 1, 3 do
+        exist:incmk(T34.new(i .. "y"), -15)
+      end
+    end
+  end
+  
+  if status == 5 then
+    if rw == 1 then
+      exist:incmk(T34.new("1f"), 144)
+      exist:incmk(T34.new("2f"), -15)
+      exist:incmk(T34.new("3f"), -15)
+      exist:incmk(T34.new("4f"), -15)
+      for i = 1, 3 do
+        exist:incmk(T34.new(i .. "y"), -15)
+      end
+    end
+    if rw == 2 then
+      exist:incmk(T34.new("2f"), 144)
+      exist:incmk(T34.new("1f"), -15)
+      exist:incmk(T34.new("3f"), -15)
+      exist:incmk(T34.new("4f"), -15)
+      for i = 1, 3 do
+        exist:incmk(T34.new(i .. "y"), -15)
+      end
+    end
+    if rw >= 3 then
+      exist:incmk(T34.new("3f"), 144)
+      exist:incmk(T34.new("2f"), -15)
+      exist:incmk(T34.new("1f"), -15)
+      exist:incmk(T34.new("4f"), -15)
+      for i = 1, 3 do
+        exist:incmk(T34.new(i .. "y"), -15)
+      end
+    end
+  end
+  
+  if status >= 6 then
+    for i = 2, 8 do
+      exist:incmk(T34.new(i .. "p"), 45)
+      exist:incmk(T34.new(i .. "s"), 45)
+      exist:incmk(T34.new(i .. "m"), 45)
+    end
   end
 end
 
 function checkinit()
-  local ok = 0
+  junme = 0
+  local sw = game:getselfwind(self)
+  local rw = game:getroundwind()
   
-  if bakuhatsu == 0 or iter > 500 then
+  if iter > 99 then
+    return true
+  end
+
+  if who ~= self then
     return true
   end
   
-  for _, t in ipairs(T34.all) do
-    if init:ct(t) == 4 then
-      ok = 1
+  if status <= 1 then
+    return init:step4() <= 3 and init:step7() == 4 and init:ct(T34.new("1y")) == 2
+  end
+
+  if status == 2 then
+    return init:step4() <= 3 and init:step7() == 4 and init:ct(T34.new("2y")) == 2
+  end
+
+  if status == 3 then
+    return init:step4() <= 3 and init:step7() == 4 and init:ct(T34.new("3y")) == 2
+  end
+
+  if status == 4 then
+    if sw == 1 then
+      return init:step4() <= 3 and init:step7() == 4 and init:ct(T34.new("1f")) == 2
+    end
+    if sw == 2 then
+      return init:step4() <= 3 and init:step7() == 4 and init:ct(T34.new("2f")) == 2
+    end
+    if sw == 3 then
+      return init:step4() <= 3 and init:step7() == 4 and init:ct(T34.new("3f")) == 2
+    end
+    if sw == 4 then
+      return init:step4() <= 3 and init:step7() == 4 and init:ct(T34.new("4f")) == 2
     end
   end
-  
+
+  if status == 5 then
+    if rw == 1 then
+      return init:step4() <= 3 and init:step7() == 4 and init:ct(T34.new("1f")) == 2
+    end
+    if rw == 2 then
+      return init:step4() <= 3 and init:step7() == 4 and init:ct(T34.new("2f")) == 2
+    end
+    if rw == 3 then
+      return init:step4() <= 3 and init:step7() == 4 and init:ct(T34.new("3f")) == 2
+    end
+  end
+
+  if status >= 6 then
   local ny = init:ct(T34.new("1m")) + init:ct(T34.new("9m")) + init:ct(T34.new("1p")) + init:ct(T34.new("9p")) + init:ct(T34.new("1s")) + init:ct(T34.new("9s")) + init:ct(T34.new("1f")) + init:ct(T34.new("2f")) + init:ct(T34.new("3f")) + init:ct(T34.new("4f")) + init:ct(T34.new("1y")) + init:ct(T34.new("2y")) + init:ct(T34.new("3y"))
-  local onone = init:ct(T34.new("1f")) <= 1 and init:ct(T34.new("2f")) <= 1 and init:ct(T34.new("3f")) <= 1 and init:ct(T34.new("4f")) <= 1 and init:ct(T34.new("1y")) <= 1 and init:ct(T34.new("2y")) <= 1 and init:ct(T34.new("3y")) <= 1
-  
-  return onone and ny >= 3 and ok == 0
+    return ny <= 2 and init:step4() <= 4 and init:step7() == 5
+  end
 end
 
 function ondraw()
-  local hand = game:gethand(who)
+  local drids = mount:getdrids()
   local hands = game:gethand(self)
   local handr = game:gethand(self:right())
   local handc = game:gethand(self:cross())
   local handl = game:gethand(self:left())
-  local formctx = game:getformctx(self)
-  local rule = game:getrule()
-  local drids = mount:getdrids()
+  local steps = hands:step(self)
+  local junmk = junme * 5
+
+  for _, t in ipairs(drids) do
+    nd = hands:ctaka5() + hands:ct(t:dora())
+  end
+
+  local dormk = ((nd - 2) * -45) - (4 * junme) - 24
   
   if rinshan then
     return
   end
-  
-  if bakuhatsu ~= 0 then
-    if hand:ct(T34.new("1f")) ~= 0 then
-      mount:lighta(T34.new("1f"), -500)
-    end
-    if hand:ct(T34.new("2f")) ~= 0 then
-      mount:lighta(T34.new("2f"), -500)
-    end
-    if hand:ct(T34.new("3f")) ~= 0 then
-      mount:lighta(T34.new("3f"), -500)
-    end
-    if hand:ct(T34.new("4f")) ~= 0 then
-      mount:lighta(T34.new("4f"), -500)
-    end
-    if hand:ct(T34.new("1y")) ~= 0 then
-      mount:lighta(T34.new("1y"), -500)
-    end
-    if hand:ct(T34.new("2y")) ~= 0 then
-      mount:lighta(T34.new("2y"), -500)
-    end
-    if hand:ct(T34.new("3y")) ~= 0 then
-      mount:lighta(T34.new("3y"), -500)
-    end
-    for _, t in ipairs(T34.all) do
-      if hand:ct(t) == 3 then
-        mount:lighta(t, -500)
-        mount:lightb(t, -1000)
-      end
-    end
-    if who == self then
-      junme = junme + 1
-      for _, t in ipairs(hands:effa()) do
-        mount:lighta(t, junme * 5)
-      end
-      for _, t in ipairs(T34.all) do
-        if hands:ct(t) == 3 then
-          mount:lighta(t, -500)
-        end
-      end
-      if hands:step() == 0 then
-        if handr:step() == 0 then
-          for _, t in ipairs(handr:effa()) do
-            mount:lighta(t, -500)
-            mount:lightb(t, -1000)
-          end
-        end
-        if handc:step() == 0 then
-          for _, t in ipairs(handc:effa()) do
-            mount:lighta(t, -500)
-            mount:lightb(t, -1000)
-          end
-        end
-        if handl:step() == 0 then
-          for _, t in ipairs(handl:effa()) do
-            mount:lighta(t, -500)
-            mount:lightb(t, -1000)
-          end
-        end
+
+  if who ~= self then
+    local river = game:getriver(who)
+    if steps >= 1 then
+      for _, t in ipairs(river) do
+        mount:lighta(t, 18)
       end
     else
-      if hand:step() == 0 then
-        for _, t in ipairs(hand:effa()) do
-          mount:lighta(t, -500)
-          mount:lightb(t, -1000)
-        end
+      for _, t in ipairs(river) do
+        mount:lighta(t, -18)
       end
-    end
-  else
-    if who == self then
-      junme = junme + 1
-      if hands:step() ~= 0 then
-        for _, t in ipairs(hands:effa()) do
-          mount:lighta(t, junme * 4)
-        end
-        mount:lighta(T37.new("0p"), 250)
-        mount:lighta(T37.new("0s"), 250)
-        mount:lighta(T37.new("0m"), 250)
-        for _, t in ipairs(drids) do
-          mount:lighta(t:dora(), 250)
-        end
-      else
-        if formctx.riichi ~= 0 then
-          for _, t in ipairs(hands:effa()) do
-          local form = Form.new(hands, formctx, rule)
-            if form:han() >= 6 then
-              mount:lighta(t, 1000)
-            end
-          end
-        else
-          for _, t in ipairs(hands:effa()) do
-            mount:lighta(t, 96 - junme * 4)
-          end
-        end
+      for _, t in ipairs(game:gethand(who):effa()) do
+        mount:lighta(t, -24)
+      end
+      for i = 3, 7 do
+        mount:lighta(T34.new(i .. "m"), 9)
+        mount:lighta(T34.new(i .. "p"), 9)
+        mount:lighta(T34.new(i .. "s"), 9)
       end
     end
   end
-end
-
-function ongameevent()
-  if event.type == "drawn" then
-    read(mount, game, who)
-  end
-end
-
-function read(mount, game, who)
-  local hands = game:gethand(self)
-  local handl = game:gethand(self:left())
-  local handc = game:gethand(self:cross())
-  local handr = game:gethand(self:right())
-  local mount = game:getmount()
-  local drids = mount:getdrids()
-  local ctxs = game:getformctx(self)
   
-  if event.args.who == self then
+  if who == self then
+    junme = junme + 1
     if handr:ready() then
-      if bakuhatsu == 0 then
-        print("下家聽牌了")
-      else
-        for _, t in ipairs(handr:effa()) do
-          print(t, "會銃下家")
-        end
+      for _, t in ipairs(handr:effa()) do
+        mount:lighta(t, -27)
       end
     end
     if handc:ready() then
-      if bakuhatsu == 0 then
-        print("對家聽牌了")
-      else
-        for _, t in ipairs(handc:effa()) do
-          print(t, "會銃對家")
-        end
+      for _, t in ipairs(handc:effa()) do
+        mount:lighta(t, -27)
       end
     end
     if handl:ready() then
-      if bakuhatsu == 0 then
-        print("上家聽牌了")
-      else
-        for _, t in ipairs(handl:effa()) do
-          print(t, "會銃上家")
-        end
+      for _, t in ipairs(handl:effa()) do
+        mount:lighta(t, -27)
+      end
+    end
+    if steps >= 1 then
+      mount:lighta(T37.new("0p"), dormk)
+      mount:lighta(T37.new("0s"), dormk)
+      mount:lighta(T37.new("0m"), dormk)
+    else
+      for _, t in ipairs(hands:effa()) do
+        mount:lighta(t, 113 - junmk)
+      end
+      for i = 2, 8 do
+        mount:lighta(T34.new(i .. "m"), -9)
+        mount:lighta(T34.new(i .. "p"), -9)
+        mount:lighta(T34.new(i .. "s"), -9)
       end
     end
   end
+  
+  ryou(mount, game, who)
 end
+
+function ryou (mount, game, who)
+  
+  if who ~= self then
+    return
+  end
+
+  local mk = 15 * junme
+  local hand = game:gethand(self)
+  local handl = game:gethand(self:left())
+  local handc = game:gethand(self:cross())
+  local handr = game:gethand(self:right())
+  local closed = hand:closed()
+  local dream = Hand.new(hand)
+  local allpair = {}
+  local allrange = 1
+  local cutpair = {}
+  local cutrange = 1
+  local bestpair = T34.new("1p")
+  local bestcut = T34.new("1p")
+  local bestcount = 0
+  local suits = { "m", "p", "s" }
+  local threeseven = {"1p","2p","3p","4p","5p","0p","6p","7p","8p","9p","1s","2s","3s","4s","5s","0s","6s","7s","8s","9s","1m","2m","3m","4m","5m","0m","6m","7m","8m","9m","1f","2f","3f","4f","1y","2y","3y"}
+
+  for _, i in ipairs(threeseven) do
+    t = T37.new(i)
+    if closed:ct(t) >= 1 then
+       cutpair[cutrange] = t
+       cutrange = cutrange + 1
+    end
+  end
+
+  for _, t in ipairs(hand:effa4 ()) do
+    allpair[allrange] = T37.new(t:id34())
+    allrange = allrange + 1
+    if t == T34.new("5p") then
+      allpair[allrange] = T37.new("0p")
+      allrange = allrange + 1
+    end
+    if t == T34.new("5s") then
+      allpair[allrange] = T37.new("0s")
+      allrange = allrange + 1
+    end
+    if t == T34.new("5m") then
+      allpair[allrange] = T37.new("0m")
+      allrange = allrange + 1
+    end
+  end
+
+  if not hand:ready() then
+    for _, t in ipairs(allpair) do
+      for _, cut in ipairs(cutpair) do
+        if mount:remaina(t) > 0 and cut ~= t then
+          dream = Hand.new(hand)
+          dream:draw(t)
+          dream:swapout(cut)
+          if dream:step() < hand:step() then
+            local comingcount = 0
+            for _, no in ipairs(dream:effa()) do
+              comingcount = comingcount + 1
+            end
+            if comingcount > bestcount and (handr:step() ~= 0 or (handr:step() == 0 and cut ~= handr:effa())) and (handc:step() ~= 0 or (handc:step() == 0 and cut ~= handc:effa())) and (handl:step() ~= 0 or (handl:step() == 0 and cut ~= handl:effa())) then
+              bestpair = t
+              bestcut = cut
+              bestcount = comingcount
+            end
+          end
+        end
+      end
+    end
+    
+    mount:lighta(bestpair, mk)
+  
+    if hand:step() == 1 and bestcount >= 3 then
+      mount:lighta(bestpair, 5 * mk)
+    end
+  end
+end 
