7fc39c37c5f174b6d645fabf1d41cc53fc8cac84
suturation399 <33090516+suturation399@users.noreply.github.com>
2020-09-30 23:36:00 +0800

    Update touka.girl.lua

--- touka.girl.lua
+++ touka.girl.lua
@@ -102,7 +102,7 @@ function ondraw()
       else
         for _, t in ipairs(hands:effa()) do
           local form = Form.new(hand, T37.new(t:id34()), ctx, rule, drids)
-          if ctx.riichi ~= 0 and form:han() >= 5 then
+          if ctx.riichi ~= 0 and form:han() >= 6 and ctx.ippatsu then
             mount:lighta(t, 1000)
           else
             mount:lighta(t, 96 - junme * 4)
