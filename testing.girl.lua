be0655f221b2655b2b88f5043ca345a684be2dc3
suturation399 <33090516+suturation399@users.noreply.github.com>
2021-08-20 01:23:15 +0800

    Update testing.girl.lua

--- testing.girl.lua
+++ testing.girl.lua
@@ -1,65 +1,15 @@
-ok = 0
-
-function checkinit()
+function onmonkey()
   junme = 0
-  local sw = game:getselfwind(self)
-  local rw = game:getroundwind()
-  ok = 0
-  
-  if who ~= self or iter > 87 then
-    return true
-  end
-  
-  if init:ct(T34.new("1y")) >= 2 and init:step4() >= 3 and init:step4() <= 4 then
-    ok = 1
-  end
-  
-  if init:ct(T34.new("2y")) >= 2 and init:step4() >= 3 and init:step4() <= 4 then
-    ok = 1
-  end
-    
-  if init:ct(T34.new("3y")) >= 2 and init:step4() >= 3 and init:step4() <= 4 then
-    ok = 1
-  end
-  
-  if rw == 1 or sw == 1 then
-    if init:ct(T34.new("1f")) >= 2 and init:step4() >= 3 and init:step4() <= 4 then
-      ok = 1
-    end
-  end
-  
-  if rw == 2 or sw == 2 then
-    if init:ct(T34.new("2f")) >= 2 and init:step4() >= 3 and init:step4() <= 4 then
-      ok = 1
-    end
-  end
-  
-  if rw == 3 or sw == 3 then
-    if init:ct(T34.new("3f")) >= 2 and init:step4() >= 3 and init:step4() <= 4 then
-      ok = 1
-    end
-  end
-  
-  if sw == 4 then
-    if init:ct(T34.new("4f")) >= 2 and init:step4() >= 3 and init:step4() <= 4 then
-      ok = 1
-    end
-  end
-  
-  local ny = init:ct(T34.new("1m")) + init:ct(T34.new("9m")) + init:ct(T34.new("1p")) + init:ct(T34.new("9p")) + init:ct(T34.new("1s")) + init:ct(T34.new("9s")) + init:ct(T34.new("1f")) + init:ct(T34.new("2f")) + init:ct(T34.new("3f")) + init:ct(T34.new("4f")) + init:ct(T34.new("1y")) + init:ct(T34.new("2y")) + init:ct(T34.new("3y"))
-  
-  if ny <= 3 and init:step() >= 3 and init:step() <= 4 then
-    ok = 1
-  end
-  
-  return ok >= 1
 end
 
 function ondraw()
   local drids = mount:getdrids()
   local hands = game:gethand(self)
+  local handr = game:gethand(self:right())
+  local handc = game:gethand(self:cross())
+  local handl = game:gethand(self:left())
   local steps = hands:step(self)
-  local junmk = junme * 4
+  local junmk = junme * 6
   local ctx = game:getformctx(self)
 
   for _, t in ipairs(drids) do
@@ -68,12 +18,63 @@ function ondraw()
 
   local dormk = ((nd - 2) * -45) - (4 * junme) - 24
   
-  if who ~= self or rinshan or ctx.ippatsu then
+  if rinshan or ctx.ippatsu then
     return
   end
 
+  if who ~= self then
+    if who == self:right() then
+      for _, t in ipairs(handl:effa()) do
+        mount:lighta(t, 62)
+      end
+    end
+    if who == self:cross() then
+      for _, t in ipairs(handr:effa()) do
+        mount:lighta(t, 62)
+      end
+    end
+    if who == self:left() then
+      for _, t in ipairs(handc:effa()) do
+        mount:lighta(t, 62)
+      end
+    end
+  end
+    
   if who == self then
     junme = junme + 1
+    if handr:ready() then
+      for _, t in ipairs(handr:effa()) do
+        for _, ea in ipairs(hands:effa()) do
+          if t == ea then
+            mount:lighta(t, 31 * 6)
+          else
+            mount:lighta(t, -31)
+          end
+        end
+      end
+    end
+    if handc:ready() then
+      for _, t in ipairs(handc:effa()) do
+        for _, ea in ipairs(hands:effa()) do
+          if t == ea then
+            mount:lighta(t, 31 * 6)
+          else
+            mount:lighta(t, -31)
+          end
+        end
+      end
+    end
+    if handl:ready() then
+      for _, t in ipairs(handl:effa()) do
+        for _, ea in ipairs(hands:effa()) do
+          if t == ea then
+            mount:lighta(t, 31 * 6)
+          else
+            mount:lighta(t, -31)
+          end
+        end
+      end
+    end
     if steps >= 1 then
       mount:lighta(T37.new("0p"), dormk)
       mount:lighta(T37.new("0s"), dormk)
@@ -86,7 +87,52 @@ function ondraw()
       end
     else
       for _, t in ipairs(hands:effa()) do
-        mount:lighta(t, 96 - junmk)
+        mount:lighta(t, 122 - junmk)
+      end
+    end
+  end
+end
+
+function ongameevent()
+  if event.type == "drawn" then
+    read(mount, game, who)
+  end
+end
+
+function ongameevent()
+  if event.type == "drawn" then
+    read(mount, game, who)
+  end
+end
+
+function read(mount, game, who)
+  local hands = game:gethand(self)
+  local handl = game:gethand(self:left())
+  local handc = game:gethand(self:cross())
+  local handr = game:gethand(self:right())
+  local mount = game:getmount()
+  local drids = mount:getdrids()
+  local ctxs = game:getformctx(self)
+  
+  if event.args.who == self then
+    if hands:step() <= 1 then
+      for _, t in ipairs(hands:effa()) do
+        print(t, "應該山存", mount:remaina(t))
+      end
+    end
+    if handr:ready() then
+      for _, t in ipairs(handr:effa()) do
+        print(t, "會銃下家，應該山存", mount:remaina(t))
+      end
+    end
+    if handc:ready() then
+      for _, t in ipairs(handc:effa()) do
+        print(t, "會銃對家，應該山存", mount:remaina(t))
+      end
+    end
+    if handl:ready() then
+      for _, t in ipairs(handl:effa()) do
+        print(t, "會銃上家，應該山存", mount:remaina(t))
       end
     end
   end
