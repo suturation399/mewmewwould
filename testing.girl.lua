d589fb3eb9c8f255cf7f302ffe9587052c4b51df
suturation399 <33090516+suturation399@users.noreply.github.com>
2023-01-22 14:19:10 +0800

    Update testing.girl.lua

--- testing.girl.lua
+++ testing.girl.lua
@@ -1,220 +1,157 @@
-function onmonkey()
-  junme = 2
-end
+ok = 0
 
 function checkinit()
-  if who ~= self or iter > 25 then
+  junme = 0
+  local sw = game:getselfwind(self)
+  local rw = game:getroundwind()
+  ok = 0
+  
+  if who ~= self or iter > 39 then
     return true
   end
   
-  return init:step() <= 4
+  if init:ct(T34.new("1y")) >= 2 and init:step4() <= 4 then
+    ok = 1
+  end
+  
+  if init:ct(T34.new("2y")) >= 2 and init:step4() <= 4 then
+    ok = 1
+  end
+    
+  if init:ct(T34.new("3y")) >= 2 and init:step4() <= 4 then
+    ok = 1
+  end
+  
+  if rw == 1 or sw == 1 then
+    if init:ct(T34.new("1f")) >= 2 and init:step4() <= 4 then
+      ok = 1
+    end
+  end
+  
+  if rw == 2 or sw == 2 then
+    if init:ct(T34.new("2f")) >= 2 and init:step4() <= 4 then
+      ok = 1
+    end
+  end
+  
+  if rw == 3 or sw == 3 then
+    if init:ct(T34.new("3f")) >= 2 and init:step4() <= 4 then
+      ok = 1
+    end
+  end
+  
+  if sw == 4 then
+    if init:ct(T34.new("4f")) >= 2 and init:step4() <= 4 then
+      ok = 1
+    end
+  end
+  
+  local ny = init:ct(T34.new("1m")) + init:ct(T34.new("9m")) + init:ct(T34.new("1p")) + init:ct(T34.new("9p")) + init:ct(T34.new("1s")) + init:ct(T34.new("9s")) + init:ct(T34.new("1f")) + init:ct(T34.new("2f")) + init:ct(T34.new("3f")) + init:ct(T34.new("4f")) + init:ct(T34.new("1y")) + init:ct(T34.new("2y")) + init:ct(T34.new("3y"))
+  
+  if ny <= 3 and init:step() <= 4 then
+    ok = 1
+  end
+  
+  return ok >= 1
 end
 
 function ondraw()
-  local junmk = junme * 5
+  local drids = mount:getdrids()
   local hands = game:gethand(self)
-  local handr = game:gethand(self:right())
-  local handc = game:gethand(self:cross())
   local handl = game:gethand(self:left())
+  local handc = game:gethand(self:cross())
+  local handr = game:gethand(self:right())
+  local steps = hands:step(self)
+  local junmk = junme * 3
   
-  if rinshan or who ~= self then
+  if who ~= self or rinshan then
     return
   end
   
-  if handr:ready() then
-    for _, t in ipairs(handr:effa()) do
-      print("下家聽牌", t)
+  if who == self then
+    junme = junme + 1
+    for _, eas in ipairs(hands:effa()) do
+      for _, ear in ipairs(handr:effa()) do
+        if eas == ear then
+          if mount:remaina(eas) > 1 then
+            mount:lighta(eas, junmk * 3)
+          else
+            mount:lighta(eas, junmk * 9)
+          end
+        else
+          mount:lighta(eas, junmk)
+          mount:lighta(ear, junmk * -1)
+        end
+      end
+      for _, eac in ipairs(handc:effa()) do
+        if eas == eac then
+          if mount:remaina(eas) > 1 then
+            mount:lighta(eas, junmk * 3)
+          else
+            mount:lighta(eas, junmk * 9)
+          end
+        else
+          mount:lighta(eas, junmk)
+          mount:lighta(eac, junmk * -1)
+        end
+      end
+      for _, eal in ipairs(handl:effa()) do
+        if eas == eal then
+          if mount:remaina(eas) > 1 then
+            mount:lighta(eas, junmk * 3)
+          else
+            mount:lighta(eas, junmk * 9)
+          end
+        else
+          mount:lighta(eas, junmk)
+          mount:lighta(eal, junmk * -1)
+        end
+      end
     end
-  else
-    print("下家向聽數", handr:step())
   end
-  if handc:ready() then
-    for _, t in ipairs(handc:effa()) do
-      print("對家聽牌", t)
-    end
-  else
-    print("對家向聽數", handc:step())
-  end
-  if handl:ready() then
-    for _, t in ipairs(handl:effa()) do
-      print("上家聽牌", t)
-    end
-  else
-    print("上家向聽數", handl:step())
-  end
-  if hands:ready() then
-    for _, t in ipairs(hands:effa()) do
-      mount:lighta(t, 110 - junmk)
-    end
-  else
-    for _, t in ipairs(hands:effa()) do
-      mount:lighta(t, junmk)
-    end
-  end
-  war(mount, game, who)
 end
 
-function war (mount, game, who)
-  if who ~= self then
-    return
+function ongameevent()
+  if event.type == "drawn" then
+    read(mount, game, who)
   end
+end
 
-  local mk = 15 * junme
-  local hand = game:gethand(self)
+function read(mount, game, who)
+  local hands = game:gethand(self)
   local handl = game:gethand(self:left())
   local handc = game:gethand(self:cross())
   local handr = game:gethand(self:right())
-  local closed = hand:closed()
-  local dream = Hand.new(hand)
-  local allpair = {}
-  local allrange = 1
-  local cutpair = {}
-  local cutrange = 1
-  local bestpair = T34.new("1p")
-  local worstpair = T34.new("1p")
-  local bestcut = T34.new("1p")
-  local worstcut = T34.new("1p")
-  local bestcount = 0
-  local suits = { "m", "p", "s" }
-  local threeseven = {"1p","2p","3p","4p","5p","0p","6p","7p","8p","9p","1s","2s","3s","4s","5s","0s","6s","7s","8s","9s","1m","2m","3m","4m","5m","0m","6m","7m","8m","9m","1f","2f","3f","4f","1y","2y","3y"}
-
-  for _, i in ipairs(threeseven) do
-    t = T37.new(i)
-    if closed:ct(t) >= 1 then
-       cutpair[cutrange] = t
-       cutrange = cutrange + 1
-    end
-  end
-
-  for _, t in ipairs(hand:effa4 ()) do
-    allpair[allrange] = T37.new(t:id34())
-    allrange = allrange + 1
-    if t == T34.new("5p") then
-      allpair[allrange] = T37.new("0p")
-      allrange = allrange + 1
-    end
-    if t == T34.new("5s") then
-      allpair[allrange] = T37.new("0s")
-      allrange = allrange + 1
-    end
-    if t == T34.new("5m") then
-      allpair[allrange] = T37.new("0m")
-      allrange = allrange + 1
-    end
-  end
+  local mount = game:getmount()
+  local drids = mount:getdrids()
   
-  for _, t in ipairs(handr:effa()) do
-    for _, ea in ipairs(hand:effa()) do
-      if t == ea then
-        mount:lighta(t, 35 * mk)
-      else
-        mount:lighta(t, -35)
+  if event.args.who == self then
+    if handr:step() <= 1 then
+      print("下家感覺", handr:step(), "向聽了")
+      for _, t in ipairs(handr:effa()) do
+        local ntenpair = mount:remaina(t)
+        print("下家有效牌", t, "山存", ntenpair, "枚")
+      end
+    end
+    if handc:step() <= 1 then
+      print("對家感覺", handc:step(), "向聽了")
+      for _, t in ipairs(handc:effa()) do
+        local ntenpaic = mount:remaina(t)
+        print("對家有效牌", t, "山存", ntenpaic, "枚")
+      end
+    end
+    if handl:step() <= 1 then
+      print("上家感覺", handl:step(), "向聽了")
+      for _, t in ipairs(handl:effa()) do
+        local ntenpail = mount:remaina(t)
+        print("上家有效牌", t, "山存", ntenpail, "枚")
+      end
+    end
+    if hands:step() <= 1 then
+      for _, t in ipairs(hands:effa()) do
+        local ntenpai = mount:remaina(t)
+        print("自家有效牌", t, "山存", ntenpai, "枚")
       end
     end
   end
-  if handc:ready() then
-    for _, t in ipairs(handc:effa()) do
-      for _, ea in ipairs(hand:effa()) do
-        if t == ea then
-          mount:lighta(t, 35 * mk)
-        else
-          mount:lighta(t, -35)
-        end
-      end
-    end
-  end
-  if handl:ready() then
-    for _, t in ipairs(handl:effa()) do
-      for _, ea in ipairs(hand:effa()) do
-        if t == ea then
-          mount:lighta(t, 35 * mk)
-        else
-          mount:lighta(t, -35)
-        end
-      end
-    end
-  end
-  
-  if not hand:ready() then
-    for _, t in ipairs(allpair) do
-      for _, cut in ipairs(cutpair) do
-        if mount:remaina(t) > 0 and cut ~= t then
-          dream = Hand.new(hand)
-          dream:draw(t)
-          dream:swapout(cut)
-          if dream:step() < hand:step() then
-            local comingcount = 0
-            for _, no in ipairs(dream:effa()) do
-              comingcount = comingcount + 1
-            end
-            if handr:step() ~= 0 and handl:step() ~= 0 and handc:step() ~= 0 then
-              if comingcount > bestcount then
-                bestpair = t
-                bestcut = cut
-                bestcount = comingcount
-              end
-            else
-              if cut ~= handr:effa() then
-                bestpair = t
-                bestcut = cut
-              end
-              if handc:step() == 0 then
-                if cut ~= handc:effa() then
-                  bestpair = t
-                  bestcut = cut
-                end
-              end
-              if handl:step() == 0 then
-                if cut ~= handl:effa() then
-                  bestpair = t
-                  bestcut = cut
-                end
-              end
-            end
-          end
-          if dream:step() <= hand:step() then
-            local comingcount = 0
-            for _, no in ipairs(dream:effa()) do
-              comingcount = comingcount + 1
-            end
-            if comingcount > bestcount then
-              if cut == handr:effa() then
-                worstpair = t
-                worstcut = cut
-              end
-              if handc:step() == 0 then
-                if cut == handc:effa() then
-                  worstpair = t
-                  worstcut = cut
-                end
-              end
-              if handl:step() == 0 then
-                if cut == handl:effa() then
-                  worstpair = t
-                  worstcut = cut
-                end
-              end
-            end
-          end
-        end
-      end
-    end
-  
-    if handr:step() ~= 0 and handl:step() ~= 0 and handc:step() ~= 0 then
-      for i = 2,8 do
-        for _, suit in ipairs(suits) do
-          if hand:step() >= 1 and closed:ct(T34.new(i .. suit)) == 2 and (closed:ct(T34.new(i+1 .. suit)) > 0 and closed:ct(T34.new(i-1 .. suit)) > 0) then
-            mount:lighta(T34.new(i .. suit), mk * 0.5)
-          end
-        end
-      end
-    end
-
-    mount:lighta(bestpair, mk)
-    mount:lighta(worstpair, -3 * mk)
-  
-    if hand:step() == 1 and bestcount >= 3 then
-      mount:lighta(bestpair, 5 * mk)
-    end
-  end
-end 
+end
