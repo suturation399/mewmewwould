e302d0408d2afe8f9a2caae49d1e14d035fcd83c
suturation399 <33090516+suturation399@users.noreply.github.com>
2023-01-22 19:59:52 +0800

    Update testing.girl.lua

--- testing.girl.lua
+++ testing.girl.lua
@@ -10,45 +10,45 @@ function checkinit()
     return true
   end
   
-  if init:ct(T34.new("1y")) >= 2 and init:step4() <= 4 then
+  if init:ct(T34.new("1y")) >= 2 and init:step4() <= 5 then
     ok = 1
   end
   
-  if init:ct(T34.new("2y")) >= 2 and init:step4() <= 4 then
+  if init:ct(T34.new("2y")) >= 2 and init:step4() <= 5 then
     ok = 1
   end
     
-  if init:ct(T34.new("3y")) >= 2 and init:step4() <= 4 then
+  if init:ct(T34.new("3y")) >= 2 and init:step4() <= 5 then
     ok = 1
   end
   
   if rw == 1 or sw == 1 then
-    if init:ct(T34.new("1f")) >= 2 and init:step4() <= 4 then
+    if init:ct(T34.new("1f")) >= 2 and init:step4() <= 5 then
       ok = 1
     end
   end
   
   if rw == 2 or sw == 2 then
-    if init:ct(T34.new("2f")) >= 2 and init:step4() <= 4 then
+    if init:ct(T34.new("2f")) >= 2 and init:step4() <= 5 then
       ok = 1
     end
   end
   
   if rw == 3 or sw == 3 then
-    if init:ct(T34.new("3f")) >= 2 and init:step4() <= 4 then
+    if init:ct(T34.new("3f")) >= 2 and init:step4() <= 5 then
       ok = 1
     end
   end
   
   if sw == 4 then
-    if init:ct(T34.new("4f")) >= 2 and init:step4() <= 4 then
+    if init:ct(T34.new("4f")) >= 2 and init:step4() <= 5 then
       ok = 1
     end
   end
   
   local ny = init:ct(T34.new("1m")) + init:ct(T34.new("9m")) + init:ct(T34.new("1p")) + init:ct(T34.new("9p")) + init:ct(T34.new("1s")) + init:ct(T34.new("9s")) + init:ct(T34.new("1f")) + init:ct(T34.new("2f")) + init:ct(T34.new("3f")) + init:ct(T34.new("4f")) + init:ct(T34.new("1y")) + init:ct(T34.new("2y")) + init:ct(T34.new("3y"))
   
-  if ny <= 3 and init:step() <= 4 then
+  if ny <= 3 and init:step() <= 5 then
     ok = 1
   end
   
