aa69fd56e7eec034916e8777f8f1f53817b8f8fa
suturation399 <33090516+suturation399@users.noreply.github.com>
2023-01-30 11:58:42 +0800

    Create Nayuta.girl.lua

---
+++ Nayuta.girl.lua
@@ -0,0 +1,144 @@
+ok = 0
+
+function checkinit()
+  junme = 0
+  local sw = game:getselfwind(self)
+  local rw = game:getroundwind()
+  ok = 0
+  
+  if who ~= self or iter > 20 then
+    return true
+  end
+  
+  if init:ct(T34.new("1y")) >= 2 and init:step() <= 4 then
+    ok = 1
+  end
+  
+  if init:ct(T34.new("2y")) >= 2 and init:step() <= 4 then
+    ok = 1
+  end
+    
+  if init:ct(T34.new("3y")) >= 2 and init:step() <= 4 then
+    ok = 1
+  end
+  
+  if rw == 1 or sw == 1 then
+    if init:ct(T34.new("1f")) >= 2 and init:step() <= 4 then
+      ok = 1
+    end
+  end
+  
+  if rw == 2 or sw == 2 then
+    if init:ct(T34.new("2f")) >= 2 and init:step() <= 4 then
+      ok = 1
+    end
+  end
+  
+  if rw == 3 or sw == 3 then
+    if init:ct(T34.new("3f")) >= 2 and init:step() <= 4 then
+      ok = 1
+    end
+  end
+  
+  if sw == 4 then
+    if init:ct(T34.new("4f")) >= 2 and init:step() <= 4 then
+      ok = 1
+    end
+  end
+  
+  local ny = init:ct(T34.new("1m")) + init:ct(T34.new("9m")) + init:ct(T34.new("1p")) + init:ct(T34.new("9p")) + init:ct(T34.new("1s")) + init:ct(T34.new("9s")) + init:ct(T34.new("1f")) + init:ct(T34.new("2f")) + init:ct(T34.new("3f")) + init:ct(T34.new("4f")) + init:ct(T34.new("1y")) + init:ct(T34.new("2y")) + init:ct(T34.new("3y"))
+  
+  if ny <= 3 and init:step() <= 4 then
+    ok = 1
+  end
+  
+  return ok >= 1
+end
+
+function ondraw()
+  local drids = mount:getdrids()
+  local hands = game:gethand(self)
+  local handl = game:gethand(self:left())
+  local handc = game:gethand(self:cross())
+  local handr = game:gethand(self:right())
+  local steps = hands:step(self)
+  local junmk = junme * 4
+  
+  if who ~= self or rinshan then
+    return
+  end
+  
+  if who == self then
+    junme = junme + 1
+    for _, eas in ipairs(hands:effa()) do
+      if mount:remaina(eas) ~= 1 then
+        mount:lighta(eas, junmk)
+      else
+        mount:lighta(eas, junmk * 20)
+      end
+      for _, ear in ipairs(handr:effa()) do
+        if ear == eas then
+          mount:lighta(ear, junmk * 2 * (6 - handr:step()))
+        else
+          mount:lighta(ear, junmk * -2)
+        end
+      end
+      for _, eac in ipairs(handc:effa()) do
+        if eac == eas then
+          mount:lighta(eac, junmk * 2 * (6 - handc:step()))
+        else
+          mount:lighta(eac, junmk * -2)
+        end
+      end
+      for _, eal in ipairs(handl:effa()) do
+        if eal == eas then
+          mount:lighta(eal, junmk * 2 * (6 - handl:step()))
+        else
+          mount:lighta(eal, junmk * -2)
+        end
+      end
+    end
+  end
+end
+
+function ongameevent()
+  if event.type == "drawn" then
+    read(mount, game, who)
+  end
+end
+
+function read(mount, game, who)
+  local hands = game:gethand(self)
+  local handl = game:gethand(self:left())
+  local handc = game:gethand(self:cross())
+  local handr = game:gethand(self:right())
+  local mount = game:getmount()
+  local drids = mount:getdrids()
+  
+  if event.args.who == self then
+    if handr:step() < 1 then
+      for _, t in ipairs(handr:effa()) do
+        local ntenpair = mount:remaina(t)
+        print("下家聽牌", t, "山存", ntenpair, "枚")
+      end
+    end
+    if handc:step() < 1 then
+      for _, t in ipairs(handc:effa()) do
+        local ntenpaic = mount:remaina(t)
+        print("對家聽牌", t, "山存", ntenpaic, "枚")
+      end
+    end
+    if handl:step() < 1 then
+      for _, t in ipairs(handl:effa()) do
+        local ntenpail = mount:remaina(t)
+        print("上家聽牌", t, "山存", ntenpail, "枚")
+      end
+    end
+    for _, t in ipairs(hands:effa()) do
+      local ntenpai = mount:remaina(t)
+      if mount:remaina(t) == 0 then
+        print( t, "山裡應該沒有了")
+      end
+    end
+  end
+end
