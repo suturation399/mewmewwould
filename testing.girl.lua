9ea79910c2e2cd06811cea41a3333e0f6a0bc5f9
suturation399 <33090516+suturation399@users.noreply.github.com>
2023-01-30 14:06:04 +0800

    Update testing.girl.lua

--- testing.girl.lua
+++ testing.girl.lua
@@ -10,45 +10,45 @@ function checkinit()
     return true
   end
   
-  if init:ct(T34.new("1y")) >= 2 and init:step() <= 4 then
+  if init:ct(T34.new("1y")) >= 2 and init:step() <= 5 then
     ok = 1
   end
   
-  if init:ct(T34.new("2y")) >= 2 and init:step() <= 4 then
+  if init:ct(T34.new("2y")) >= 2 and init:step() <= 5 then
     ok = 1
   end
     
-  if init:ct(T34.new("3y")) >= 2 and init:step() <= 4 then
+  if init:ct(T34.new("3y")) >= 2 and init:step() <= 5 then
     ok = 1
   end
   
   if rw == 1 or sw == 1 then
-    if init:ct(T34.new("1f")) >= 2 and init:step() <= 4 then
+    if init:ct(T34.new("1f")) >= 2 and init:step() <= 5 then
       ok = 1
     end
   end
   
   if rw == 2 or sw == 2 then
-    if init:ct(T34.new("2f")) >= 2 and init:step() <= 4 then
+    if init:ct(T34.new("2f")) >= 2 and init:step() <= 5 then
       ok = 1
     end
   end
   
   if rw == 3 or sw == 3 then
-    if init:ct(T34.new("3f")) >= 2 and init:step() <= 4 then
+    if init:ct(T34.new("3f")) >= 2 and init:step() <= 5 then
       ok = 1
     end
   end
   
   if sw == 4 then
-    if init:ct(T34.new("4f")) >= 2 and init:step() <= 4 then
+    if init:ct(T34.new("4f")) >= 2 and init:step() <= 5 then
       ok = 1
     end
   end
   
   local ny = init:ct(T34.new("1m")) + init:ct(T34.new("9m")) + init:ct(T34.new("1p")) + init:ct(T34.new("9p")) + init:ct(T34.new("1s")) + init:ct(T34.new("9s")) + init:ct(T34.new("1f")) + init:ct(T34.new("2f")) + init:ct(T34.new("3f")) + init:ct(T34.new("4f")) + init:ct(T34.new("1y")) + init:ct(T34.new("2y")) + init:ct(T34.new("3y"))
   
-  if ny <= 3 and init:step() <= 4 then
+  if ny <= 3 and init:step() <= 5 then
     ok = 1
   end
   
@@ -64,8 +64,113 @@ function ondraw()
   local steps = hands:step(self)
   local junmk = junme * 4
   
-  if who ~= self or rinshan then
+  if rinshan then
     return
+  end
+  
+  if who == self:right() then
+    if handr:step() ~= 0 then
+      if handc:step() == 0 then
+        for _, eac in ipairs(handc:effa()) do
+          mount:lighta(eac, junmk * 16)
+        end
+      else
+        for _, ear in ipairs(handr:effa()) do
+          for _, eac in ipairs(handc:effa()) do
+            if ear == eac then
+              mount:lighta(ear, 16)
+            else
+              mount:lighta(ear, -2)
+            end
+          end
+        end
+      end
+      if handl:step() == 0 then
+        for _, eal in ipairs(handl:effa()) do
+          mount:lighta(eal, junmk * 16)
+        end
+      else
+        for _, ear in ipairs(handr:effa()) do
+          for _, eal in ipairs(handl:effa()) do
+            if ear == eal then
+              mount:lighta(ear, 16)
+            else
+              mount:lighta(ear, -2)
+            end
+          end
+        end
+      end
+    end
+  end
+  
+  if who == self:cross() then
+    if handc:step() ~= 0 then
+      if handr:step() == 0 then
+        for _, ear in ipairs(handr:effa()) do
+          mount:lighta(ear, junmk * 16)
+        end
+      else
+        for _, eac in ipairs(handc:effa()) do
+          for _, ear in ipairs(handr:effa()) do
+            if eac == ear then
+              mount:lighta(eac, 16)
+            else
+              mount:lighta(eac, -2)
+            end
+          end
+        end
+      end
+      if handl:step() == 0 then
+        for _, eal in ipairs(handl:effa()) do
+          mount:lighta(eal, junmk * 16)
+        end
+      else
+        for _, eac in ipairs(handc:effa()) do
+          for _, eal in ipairs(handl:effa()) do
+            if eac == eal then
+              mount:lighta(eac, 16)
+            else
+              mount:lighta(eac, -2)
+            end
+          end
+        end
+      end
+    end
+  end
+  
+  if who == self:left() then
+    if handl:step() ~= 0 then
+      if handr:step() == 0 then
+        for _, ear in ipairs(handr:effa()) do
+          mount:lighta(ear, junmk * 16)
+        end
+      else
+        for _, eal in ipairs(handl:effa()) do
+          for _, ear in ipairs(handr:effa()) do
+            if eal == ear then
+              mount:lighta(eal, 16)
+            else
+              mount:lighta(eal, -2)
+            end
+          end
+        end
+      end
+      if handc:step() == 0 then
+        for _, eac in ipairs(handc:effa()) do
+          mount:lighta(eac, junmk * 16)
+        end
+      else
+        for _, eal in ipairs(handl:effa()) do
+          for _, eac in ipairs(handc:effa()) do
+            if eal == eac then
+              mount:lighta(eal, 16)
+            else
+              mount:lighta(eal, -2)
+            end
+          end
+        end
+      end
+    end
   end
   
   if who == self then
@@ -74,27 +179,21 @@ function ondraw()
       if mount:remaina(eas) ~= 1 then
         mount:lighta(eas, junmk)
       else
-        mount:lighta(eas, junmk * 20)
+        mount:lighta(eas, junmk * 8)
       end
       for _, ear in ipairs(handr:effa()) do
-        if ear == eas then
+        if eas == ear then
           mount:lighta(ear, junmk * 2 * (6 - handr:step()))
-        else
-          mount:lighta(ear, junmk * -2)
         end
       end
       for _, eac in ipairs(handc:effa()) do
-        if eac == eas then
+        if eas == eac then
           mount:lighta(eac, junmk * 2 * (6 - handc:step()))
-        else
-          mount:lighta(eac, junmk * -2)
         end
       end
       for _, eal in ipairs(handl:effa()) do
-        if eal == eas then
+        if eas == eal then
           mount:lighta(eal, junmk * 2 * (6 - handl:step()))
-        else
-          mount:lighta(eal, junmk * -2)
         end
       end
     end
@@ -137,7 +236,7 @@ function read(mount, game, who)
     for _, t in ipairs(hands:effa()) do
       local ntenpai = mount:remaina(t)
       if mount:remaina(t) == 0 then
-        print( t, "山裡應該沒有了")
+        print( t, "我應該摸不到了")
       end
     end
   end
