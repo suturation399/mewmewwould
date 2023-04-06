2a06ccfde8aa36ce91761eafc6d83aa8d6d3d4af
suturation399 <33090516+suturation399@users.noreply.github.com>
2023-04-06 21:56:07 +0800

    Update testing.girl.lua

--- testing.girl.lua
+++ testing.girl.lua
@@ -1,5 +1,5 @@
 lv5tars = {
-  "1m", "2m", "3m", "4m", "5m", "6m", "7m", "8m", "9m", "3y" 
+  "1m", "2m", "3m", "4m", "5m", "6m", "7m", "8m", "9m", "3y",
 }
 
 lv4tars = {
@@ -11,27 +11,68 @@ lv3tars = {
 }
 
 powers = {
-  [lv5tars] = 90,
-  [lv4tars] = 60,
-  [lv3tars] = 30
+  [lv5tars] = 3,
+  [lv4tars] = 2,
+  [lv3tars] = 1,
 }
 
 function onmonkey()
+  junme = 0
+  local existself = exists[self:index()]
+  local existl = exists[self:left():index()]
+  local existc = exists[self:cross():index()]
+  local existr = exists[self:right():index()]
+  
   for tars, mk in pairs(powers) do
-    for _, t in ipairs(tars) do
-      exists[self:index()]:incmk(T34.new(t), mk)
+    for _, y in ipairs(tars) do
+      existself:incmk(T34.new(y), mk * 30)
     end
   end
+  existself:incmk(T37.new("0p"), 90)
+  existself:incmk(T37.new("0s"), 90)
+  existself:incmk(T37.new("0m"), 90)
 end
 
 function ondraw()
+  local drids = mount:getdrids()
+  local hands = game:gethand(self)
+  local steps = hands:step(self)
+  local ctx = game:getformctx(self)
+  
   if who ~= self or rinshan then
     return
   end
-
-  for tars, mk in pairs(powers) do
-    for _, t in ipairs(tars) do
-      mount:lighta(T34.new(t), mk)
+  
+  if who == self then
+    junme = junme + 1
+    if steps >= 1 then
+      for _, t in ipairs(hands:effa()) do
+        mount:lighta(T34.new(t), junme * 3)
+        for tars, mk in pairs(powers) do
+          for _, y in ipairs(tars) do
+            if y == t then
+              mount:lighta(T34.new(y), mk * junme * 4)
+            else
+              mount:lighta(T34.new(y), junme * mk)
+            end
+          end
+        end
+      end
+      mount:lighta(T37.new("0p"), 90)
+      mount:lighta(T37.new("0s"), 90)
+      mount:lighta(T37.new("0m"), 90)
+    else
+      for _, t in ipairs(hands:effa()) do
+        for tars, mk in pairs(powers) do
+          for _, y in ipairs(tars) do
+            if y == t then
+              mount:lighta(T34.new(t), 288 - mk * junme * 4)
+            else
+              mount:lighta(T34.new(t), 96 - junme * 4)
+            end
+          end
+        end
+      end
     end
   end
 end
