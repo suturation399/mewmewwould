4f86842d03274946cc1903a7b548edf2418da856
suturation399 <33090516+suturation399@users.noreply.github.com>
2023-04-22 14:21:34 +0800

    Update testing.girl.lua

--- testing.girl.lua
+++ testing.girl.lua
@@ -1,33 +1,21 @@
-lv5tars = {
-  "1m", "2m", "3m", "4m", "5m", "6m", "7m", "8m", "9m", "3y",
-}
-
-lv4tars = {
-  "6p", "7p", "9p", "9s"
-}
-
-lv3tars = {
-  "1p", "3p", "5p", "1s", "5s", "7s"
-}
-
-powers = {
-  [lv5tars] = 4,
-  [lv4tars] = 3,
-  [lv3tars] = 2,
-}
-
 function onmonkey()
   junme = 0
   local existself = exists[self:index()]
-  local existl = exists[self:left():index()]
-  local existc = exists[self:cross():index()]
-  local existr = exists[self:right():index()]
   
-  for tars, mk in pairs(powers) do
-    for _, yu in ipairs(tars) do
-      existself:incmk(T34.new(yu), mk * 30)
-    end
-  end
+  existself:incmk(T34.new("2p"), -5)
+  existself:incmk(T34.new("4p"), -5)
+  existself:incmk(T34.new("8p"), -5)
+  existself:incmk(T34.new("2s"), -5)
+  existself:incmk(T34.new("3s"), -5)
+  existself:incmk(T34.new("4s"), -5)
+  existself:incmk(T34.new("6s"), -5)
+  existself:incmk(T34.new("8s"), -5)
+  existself:incmk(T34.new("1f"), -5)
+  existself:incmk(T34.new("2f"), -5)
+  existself:incmk(T34.new("3f"), -5)
+  existself:incmk(T34.new("4f"), -5)
+  existself:incmk(T34.new("1y"), -5)
+  existself:incmk(T34.new("2y"), -5)
   existself:incmk(T37.new("0p"), 90)
   existself:incmk(T37.new("0s"), 90)
   existself:incmk(T37.new("0m"), 90)
@@ -43,30 +31,11 @@ function ondraw()
     return
   end
   
-  if who == self then
-    junme = junme + 1
-    if steps >= 1 then
-      for _, t in ipairs(hands:effa()) do
-        for _, yu in ipairs(lv5tars) do
-            if t == yu then
-              mount:lighta(T34.new(t), mk * junme * 4)
-              mount:lighta(T34.new(yu), mk * junme)
-            else
-              mount:lighta(T34.new(t), junme * 4)
-              mount:lighta(T34.new(yu), mk * junme)
-            end
-          end
-        end
-      end
-    else
-      for _, t in ipairs(hands:effa()) do
-        mount:lighta(t, 120 - junme * 5)
-      end
-      for tars, mk in pairs(powers) do
-        for _, yu in ipairs(tars) do
-          mount:lighta(T34.new(yu), mk * (24 - junme))
-        end
-      end
-    end
-  end
-end
+  mount:lighta(T37.new("0p"), 90)
+  mount:lighta(T37.new("0s"), 90)
+  mount:lighta(T37.new("0m"), 90)
+  mount:lighta(T34.new("0p"), )
+  mount:lighta(T34.new("0s"), )
+  mount:lighta(T34.new("0m"), 90)
+  junme = junme + 1
+    
